bbf9795927828bf8513466c41d0e74240abc65c0
David Korth <gerbilsoft@gerbilsoft.com>
2013-06-04 23:03:08 -0400

    Checksum: New checksum class for checksum verification.
    Currently only supports CRC-16, but I plan on adding CRC-32,
    "AddBytes32", and "SonicChaoGarden" algorithms as well.

---
+++ src/mcrecover/Checksum.cpp
@@ -0,0 +1,92 @@
+/***************************************************************************
+ * GameCube Memory Card Recovery Program.                                  *
+ * Checksum.cpp: Checksum algorithm class.                                 *
+ *                                                                         *
+ * Copyright (c) 2013 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "Checksum.hpp"
+
+class ChecksumPrivate
+{
+	private:
+		ChecksumPrivate();
+		~ChecksumPrivate();
+		ChecksumPrivate(const ChecksumPrivate &);
+		ChecksumPrivate &operator=(const ChecksumPrivate &);
+
+	public:
+		static uint16_t Crc16(const uint8_t *buf, uint32_t siz, uint16_t poly = Checksum::CRC16_POLY_CCITT);
+};
+
+
+/**
+ * CRC-16 algorithm.
+ * @param buf Data buffer.
+ * @param siz Length of data buffer.
+ * @param poly Polynomial.
+ * @return Checksum.
+ */
+uint16_t ChecksumPrivate::Crc16(const uint8_t *buf, uint32_t siz, uint16_t poly)
+{
+	// TODO: Add optimized version for poly == CRC16_POLY_CCITT.
+	const uint8_t derp[4] = {0x1B, 0xFF, 0x41, 0x00};
+	uint16_t crc = 0xFFFF;
+
+	for (; siz != 0; siz--, buf++) {
+		crc ^= (*buf & 0xFF);
+		for (int i = 8; i > 0; i--) {
+			if (crc & 1)
+				crc = ((crc >> 1) ^ poly);
+			else
+				crc >>= 1;
+		}
+	}
+
+	return ~crc;
+}
+
+
+/**
+ * Get the checksum for a block of data.
+ * @param algorithm Checksum algorithm.
+ * @param buf Data buffer.
+ * @param siz Length of data buffer.
+ * @param poly Polynomial. (Not needed for some algorithms.)
+ * @return Checksum.
+ */
+uint32_t Checksum::Exec(ChkAlgorithm algorithm, const void *buf, uint32_t siz, uint32_t poly)
+{
+	switch (algorithm) {
+		case CHKALG_CRC16:
+			if (poly == 0)
+				poly = CRC16_POLY_CCITT;
+			return ChecksumPrivate::Crc16(
+				reinterpret_cast<const uint8_t*>(buf), siz, (uint16_t)(poly & 0xFFFF));
+
+		case CHKALG_CRC32:
+		case CHKALG_ALLBYTES32:
+		case CHKALG_SONICCHAOGARDEN:
+			// TODO
+
+		default:
+			break;
+	}
+
+	// Unknown algorithm.
+	return 0;
+}
---
+++ src/mcrecover/Checksum.hpp
@@ -0,0 +1,84 @@
+/***************************************************************************
+ * GameCube Memory Card Recovery Program.                                  *
+ * Checksum.hpp: Checksum algorithm class.                                 *
+ *                                                                         *
+ * Copyright (c) 2013 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __MCRECOVER_CHECKSUM_HPP__
+#define __MCRECOVER_CHECKSUM_HPP__
+
+// C includes.
+#include <stdint.h>
+
+class Checksum
+{
+	private:
+		// Static class.
+		Checksum();
+		~Checksum();
+		Checksum(const Checksum &);
+		Checksum &operator=(const Checksum &);
+
+	public:
+		enum ChkAlgorithm {
+			CHKALG_NONE = 0,
+			CHKALG_CRC16,
+			CHKALG_CRC32,
+			CHKALG_ALLBYTES32,
+			CHKALG_SONICCHAOGARDEN,
+
+			CHKALG_MAX
+		};
+
+		// Checksum data struct.
+		struct ChecksumData {
+			ChkAlgorithm algorithm;
+			uint32_t address;	// Checksum address.
+			uint32_t poly;		// Polynomial for CRC algorithms.
+			uint32_t start;		// Checksummed area: start.
+			uint32_t length;	// Checksummed area: length.
+
+			ChecksumData()
+				{ clear(); }
+
+			void clear(void) {
+				algorithm = CHKALG_NONE;
+				address = 0;
+				poly = 0;
+				start = 0;
+				length = 0;
+			}
+		};
+
+		/** Default polynomials. **/
+
+		static const uint16_t CRC16_POLY_CCITT = 0x8408;
+		static const uint32_t CRC32_POLY_ZLIB = 0xEDB88320;
+
+		/**
+		 * Get the checksum for a block of data.
+		 * @param algorithm Checksum algorithm.
+		 * @param buf Data buffer.
+		 * @param siz Length of data buffer.
+		 * @param poly Polynomial. (Not needed for some algorithms.)
+		 * @return Checksum.
+		 */
+		static uint32_t Exec(ChkAlgorithm algorithm, const void *buf, uint32_t siz, uint32_t poly = 0);
+};
+
+#endif /* __MCRECOVER_CHECKSUM_HPP__ */
