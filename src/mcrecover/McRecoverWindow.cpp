GerbilSoft/mcrecover

commit f159689f22271030219e1ecb517b9117bfc4d266

Renamed the program to "GCN MemCard Recover".
Much shorter and easier to write than "GameCube Memory Card Recovery Program".

src/mcrecover/McRecoverWindow.cpp
@@ -490,7 +490,7 @@ void McRecoverWindow::on_actionScan_triggered(void)
 
 	// Search blocks for lost files.
 	// TODO: Handle errors.
-	ret = d->searchThread->searchMemCard_async(d->card);
+	ret = d->searchThread->searchMemCard_async(d->card, true);
 	if (ret < 0) {
 		// Error starting the thread.
 		// Use the synchronous version.

CMakeLists.txt
@@ -16,11 +16,22 @@ list (APPEND CMAKE_MODULE_PATH
 	"${mcrecover_base_SOURCE_DIR}/cmake/macros"
 	)
 
+# Program information.
+SET(DESCRIPTION "GCN MemCard Recover")
+SET(AUTHOR "David Korth")
+SET(VERSION_MAJOR "0")
+SET(VERSION_MINOR "0")
+SET(VERSION_PATCH "0")
+SET(VERSION_STRING "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}")
+SET(VERSION_STRING_WIN32 "${VERSION_MAJOR},${VERSION_MINOR},${VERSION_PATCH},0")
+
 # Git version information.
 ADD_CUSTOM_TARGET(git_version ALL
-	${mcrecover_base_SOURCE_DIR}/git_version.sh -s "${mcrecover_base_SOURCE_DIR}" -o "${mcrecover_base_BINARY_DIR}/git_version.h"
+	${mcrecover_base_SOURCE_DIR}/git_version.sh
+	-s "${mcrecover_base_SOURCE_DIR}"
+	-o "${mcrecover_base_BINARY_DIR}/git_version.h"
 	VERBATIM
-)
+	)
 
 # If no build type is set, default to "debug".
 IF(CMAKE_BUILD_TYPE MATCHES ^$)

src/mcrecover/AboutDialog.cpp
@@ -96,9 +96,11 @@ void AboutDialogPrivate::initAboutDialogText(void)
 
 	// Build the program title text.
 	// TODO
-	QString sPrgTitle =
-		QLatin1String("<b>GameCube Memory Card<br/>Recovery Program</b>") + sLineBreak +
-		AboutDialog::tr("Version %1").arg(QApplication::applicationVersion()) + sLineBreak;
+	QString sPrgTitle = QLatin1String("<b>") +
+				QApplication::applicationName() +
+				QLatin1String("</b>") + sLineBreak +
+				AboutDialog::tr("Version %1")
+				.arg(QApplication::applicationVersion()) + sLineBreak;
 
 	// Set the program title text.
         q->lblPrgTitle->setText(sPrgTitle);

src/mcrecover/CMakeLists.txt
@@ -189,7 +189,7 @@ SET_TARGET_PROPERTIES(mcrecover
 
 # Mac OS X bundle information.
 SET(MACOSX_BUNDLE_GUI_IDENTIFIER "gsft.gerbilsoft.McRecover")
-SET(MACOSX_BUNDLE_BUNDLE_NAME "GameCube Memory Card Recovery")
+SET(MACOSX_BUNDLE_BUNDLE_NAME "GCN MemCard Recover")
 #SET(MACOSX_BUNDLE_ICON_FILE "mcrecover.icns")
 SET(MACOSX_BUNDLE_SHORT_VERSION_STRING "0.0.0")
 SET(MACOSX_BUNDLE_LONG_VERSION_STRING "0.0.0")

src/mcrecover/McRecoverQApplication.cpp
@@ -76,7 +76,7 @@ void McRecoverQApplicationPrivate::mcrqaInit(void)
 {
 	// Set application information.
 	QCoreApplication::setOrganizationName(QLatin1String("GerbilSoft"));
-	QCoreApplication::setApplicationName(QLatin1String("GameCube Memory Card Recovery Program"));
+	QCoreApplication::setApplicationName(QLatin1String("GCN MemCard Recover"));
 
 	// Version number. (TODO: Get from CMake.)
 #if 0

src/mcrecover/ui/AboutDialog.ui
@@ -11,7 +11,7 @@
    </rect>
   </property>
   <property name="windowTitle">
-   <string>About GameCube Memory Card Recovery Program</string>
+   <string>About GCN MemCard Recover</string>
   </property>
   <layout class="QVBoxLayout" name="vboxDialog">
    <property name="spacing">

src/mcrecover/ui/McRecoverWindow.ui
@@ -14,7 +14,7 @@
    <bool>true</bool>
   </property>
   <property name="windowTitle">
-   <string>GameCube Memory Card Recovery Program</string>
+   <string>GCN MemCard Recover</string>
   </property>
   <widget class="QWidget" name="centralwidget">
    <layout class="QHBoxLayout" name="hboxMainLayout">

src/mcrecover/ui/resources/win32/mcrecover.exe.manifest
@@ -7,7 +7,7 @@
 		processorArchitecture="x86"
 	/>
 
-	<description>GameCube Memory Card Recovery Program</description>
+	<description>GCN MemCard Recover</description>
 
 	<!-- Common Controls 6. Needed for visual styles on XP and later. -->
 	<dependency>

commit 7753b5435ed9355aaef3ed84864fea339a1d3b86

McRecoverWindow: Don't search used blocks.
This was enabled for testing, but should not be committed to git.

This is a regression from commit f159689f22271030219e1ecb517b9117bfc4d266.
(Renamed the program to "GCN MemCard Recover".)

src/mcrecover/McRecoverWindow.cpp
@@ -501,7 +501,7 @@ void McRecoverWindow::on_actionScan_triggered(void)
 
 	// Search blocks for lost files.
 	// TODO: Handle errors.
-	ret = d->searchThread->searchMemCard_async(d->card, true);
+	ret = d->searchThread->searchMemCard_async(d->card);
 	if (ret < 0) {
 		// Error starting the thread.
 		// Use the synchronous version.