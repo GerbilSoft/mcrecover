07c0f90c814c90425061eedd8884634abd5be14a
David Korth <gerbilsoft@gerbilsoft.com>
2013-07-07 00:53:29 -0400

    Added SPLIT_DEBUG and COMPRESS_EXE options.
    SPLIT_DEBUG splits the debug information into a separate file.
    This is enabled by default.
    
    COMPRESS_EXE compresses the executable using UPX. This is usually
    only done for Win32 release builds, since Qt is statically linked
    in the Win32 release build. This is disabled by default.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,3 +2,9 @@
 
 # Link-time optimization.
 OPTION(ENABLE_LTO	"Enable link-time optimization. (Release builds only)"	0)
+
+# Split debug information into a separate file.
+OPTION(SPLIT_DEBUG	"Split debug information into a separate file."		1)
+
+# Compress the executable with UPX.
+OPTION(COMPRESS_EXE	"Compress the executable with UPX."			0)
