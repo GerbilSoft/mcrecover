GerbilSoft/mcrecover

commit b1c6307cad1561d015d50f5c392a208dc3b74eef

QTreeViewOpt: QTreeView subclass that only propagates dataChanged() if the item is onscreen.
This helps to reduce CPU usage in McRecoverWindow, since Qt usually
propagates dataChanged() even if the item being updated is offscreen.
Animated icons cause this to happen multiple times a second.

McRecoverWindow: Use QTreeViewOpt for lstFileList instead of QTreeView.

TODO: Figure out a better way to implement animated icons, such as
an embedded QLabel widget.

src/mcrecover/McRecoverWindow.cpp
@@ -785,7 +785,7 @@ void McRecoverWindow::on_actionScan_triggered(void)
 
 	// Search blocks for lost files.
 	// TODO: Handle errors.
-	ret = d->searchThread->searchMemCard_async(d->card);
+	ret = d->searchThread->searchMemCard_async(d->card, true);
 	if (ret < 0) {
 		// Error starting the thread.
 		// Use the synchronous version.

src/mcrecover/CMakeLists.txt
@@ -99,6 +99,7 @@ SET(mcrecover_SRCS
 	VarReplace.cpp
 	MemCardItemDelegate.cpp
 	FileComments.cpp
+	QTreeViewOpt.cpp
 	)
 
 # Win32-specific sources.
@@ -126,6 +127,7 @@ SET(mcrecover_MOC_HEADERS
 	McRecoverQApplication.hpp
 	AboutDialog.hpp
 	MemCardItemDelegate.hpp
+	QTreeViewOpt.hpp
 	)
 
 # UI files.

src/mcrecover/QTreeViewOpt.cpp
@@ -1,0 +1,47 @@
+/***************************************************************************
+ * GameCube Memory Card Recovery Program.                                  *
+ * QTreeViewOpt.cpp: QTreeView with drawing optimizations.                 *
+ * Specifically, don't update rows that are offscreen.			   *
+ *                                                                         *
+ * Copyright (c) 2013 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "QTreeViewOpt.hpp"
+
+void QTreeViewOpt::dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight)
+{
+	bool propagateEvent = true;
+	// TODO: Support for checking multiple items.
+	if (topLeft == bottomRight) {
+		// Single item. This might be an icon animation.
+		// If it is, make sure the icon is onscreen.
+		QRect itemRect = this->visualRect(topLeft);
+
+		// Get the viewport rect.
+		QRect viewportRect(QPoint(0, 0), this->viewport()->size());
+		if (!viewportRect.contains(itemRect)) {
+			// Item is NOT visible.
+			// Don't propagate the event.
+			propagateEvent = false;
+		}
+	}
+
+	if (propagateEvent) {
+		// Propagate the dataChanged() event.
+		QTreeView::dataChanged(topLeft, bottomRight);
+	}
+}

src/mcrecover/QTreeViewOpt.hpp
@@ -1,0 +1,44 @@
+/***************************************************************************
+ * GameCube Memory Card Recovery Program.                                  *
+ * QTreeViewOpt.hpp: QTreeView with drawing optimizations.                 *
+ * Specifically, don't update rows that are offscreen.			   *
+ *                                                                         *
+ * Copyright (c) 2013 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __MCRECOVER_QTREEVIEWOPT_HPP__
+#define __MCRECOVER_QTREEVIEWOPT_HPP__
+
+#include <QtGui/QTreeView>
+
+class QTreeViewOpt : public QTreeView
+{
+	Q_OBJECT
+
+	public:
+		QTreeViewOpt(QWidget *parent = 0)
+			: QTreeView(parent) { }
+		virtual ~QTreeViewOpt() { }
+
+	private:
+		Q_DISABLE_COPY(QTreeViewOpt);
+
+	public:
+		virtual void dataChanged(const QModelIndex &topLeft, const QModelIndex &bottomRight);
+};
+
+#endif /* __MCRECOVER_QTREEVIEWOPT_HPP__ */

src/mcrecover/ui/McRecoverWindow.ui
@@ -35,7 +35,7 @@
           </property>
           <layout class="QVBoxLayout" name="vboxGrpFileList">
            <item>
-            <widget class="QTreeView" name="lstFileList">
+            <widget class="QTreeViewOpt" name="lstFileList">
              <property name="alternatingRowColors">
               <bool>true</bool>
              </property>
@@ -308,6 +308,11 @@
    <extends>QWidget</extends>
    <header>MemCardView.hpp</header>
    <container>1</container>
+  </customwidget>
+  <customwidget>
+   <class>QTreeViewOpt</class>
+   <extends>QTreeView</extends>
+   <header>QTreeViewOpt.hpp</header>
   </customwidget>
  </customwidgets>
  <tabstops>

commit e70092f259eb21b6254bc8cbec0a0a0adf8aab7f

McRecoverWindow::on_actionScan_triggered(): Don't scan used blocks.
I enable this occasionally when debugging, but I keep forgetting
to remove it before committing. Maybe I should make this a runtime
option instead of a compile-time option.

This is a regression from commit b1c6307cad1561d015d50f5c392a208dc3b74eef.
(QTreeViewOpt: QTreeView subclass that only propagates dataChanged() if the item is onscreen.)

src/mcrecover/McRecoverWindow.cpp
@@ -785,7 +785,7 @@ void McRecoverWindow::on_actionScan_triggered(void)
 
 	// Search blocks for lost files.
 	// TODO: Handle errors.
-	ret = d->searchThread->searchMemCard_async(d->card, true);
+	ret = d->searchThread->searchMemCard_async(d->card);
 	if (ret < 0) {
 		// Error starting the thread.
 		// Use the synchronous version.