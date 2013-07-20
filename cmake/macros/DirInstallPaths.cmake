7e4ded3bfa9dd9dad6b6a4765bcb7dde8251a8c3
David Korth <gerbilsoft@gerbilsoft.com>
2013-07-19 21:10:15 -0400

    doc/: Added licenses used by GCN MemCard Recover.
    cmake/macros/DirInstallPaths.cmake: Install doc/ files.
    
    CMakeLists.txt: Added doc subdirectory target.

--- cmake/macros/DirInstallPaths.cmake
+++ cmake/macros/DirInstallPaths.cmake
@@ -5,10 +5,11 @@
 
 IF(UNIX AND NOT APPLE)
 	# Unix-style install paths.
-	SET(DIR_INSTALL_DATA "share/mcrecover/data")
 	SET(DIR_INSTALL_EXE "bin")
 	SET(DIR_INSTALL_DLL "lib")
 	SET(DIR_INSTALL_LIB "lib")
+	SET(DIR_INSTALL_DATA "share/mcrecover/data")
+	SET(DIR_INSTALL_DOC "share/doc/mcrecover")
 ELSEIF(APPLE)
 	# Mac OS X-style install paths.
 	# Install should be relative to the application bundle.
@@ -17,8 +18,9 @@ ELSEIF(WIN32)
 	# Win32-style install paths.
 	# Files are installed relative to root, since the
 	# program is run out of its own directory.
-	SET(DIR_INSTALL_DATA "data")
 	SET(DIR_INSTALL_EXE ".")
 	SET(DIR_INSTALL_DLL ".")
 	SET(DIR_INSTALL_LIB ".")
+	SET(DIR_INSTALL_DATA "data")
+	SET(DIR_INSTALL_DOC "doc")
 ENDIF()
