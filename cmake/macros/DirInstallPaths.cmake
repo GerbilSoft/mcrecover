1a034d2d21dbc3c54603c7fce859841bace68cd4
David Korth <gerbilsoft@gerbilsoft.com>
2013-07-21 10:27:24 -0400

    DirInstallPaths.cmake: Add paths for debug information.
    CMakeLists.txt: Install debug information.
    We're going to include the debug information in the main package for now
    instead of providing a separate debug package.

--- cmake/macros/DirInstallPaths.cmake
+++ cmake/macros/DirInstallPaths.cmake
@@ -10,10 +10,14 @@ IF(UNIX AND NOT APPLE)
 	SET(DIR_INSTALL_LIB "lib")
 	SET(DIR_INSTALL_DATA "share/mcrecover/data")
 	SET(DIR_INSTALL_DOC "share/doc/mcrecover")
+	SET(DIR_INSTALL_EXE_DEBUG "lib/debug/${CMAKE_INSTALL_PREFIX}/${DIR_INSTALL_EXE}")
+	SET(DIR_INSTALL_DLL_DEBUG "lib/debug/${CMAKE_INSTALL_PREFIX}/${DIR_INSTALL_DLL}")
+	SET(DIR_INSTALL_LIB_DEBUG "lib/debug/${CMAKE_INSTALL_PREFIX}/${DIR_INSTALL_LIB}")
 ELSEIF(APPLE)
 	# Mac OS X-style install paths.
 	# Install should be relative to the application bundle.
-	# (TODO)
+	# (TODO for mcrecover-v0.2)
+	MESSAGE(STATUS "WARNING: Mac OS X is not officially supported yet.")
 ELSEIF(WIN32)
 	# Win32-style install paths.
 	# Files are installed relative to root, since the
@@ -23,4 +27,7 @@ ELSEIF(WIN32)
 	SET(DIR_INSTALL_LIB ".")
 	SET(DIR_INSTALL_DATA "data")
 	SET(DIR_INSTALL_DOC "doc")
+	SET(DIR_INSTALL_EXE_DEBUG "debug")
+	SET(DIR_INSTALL_DLL_DEBUG "debug")
+	SET(DIR_INSTALL_LIB_DEBUG "debug")
 ENDIF()
