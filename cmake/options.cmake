649f392790b39559f72f56c49ae2c0051d41fde9
David Korth <gerbilsoft@gerbilsoft.com>
2013-07-21 10:36:13 -0400

    cmake/options.cmake: Added an option for installing the split debug file.
    CPack doesn't easily support generating multiple packages unless I
    (ab)use the COMPONENT feature, which I'm not going to bother with
    right now. So, add an option to not install the debug file.
    
    I'll package the debug file separately for each platform.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -6,5 +6,8 @@ OPTION(ENABLE_LTO	"Enable link-time optimization. (Release builds only)"	0)
 # Split debug information into a separate file.
 OPTION(SPLIT_DEBUG	"Split debug information into a separate file."		1)
 
+# Install the split debug file.
+OPTION(INSTALL_DEBUG	"Install the split debug file."				1)
+
 # Compress the executable with UPX.
 OPTION(COMPRESS_EXE	"Compress the executable with UPX."			0)
