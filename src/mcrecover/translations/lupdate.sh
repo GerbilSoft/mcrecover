d11fda7aac1dd50908ec48d1f04b6994099db129
David Korth <gerbilsoft@gerbilsoft.com>
2014-01-06 20:30:15 -0500

    Added translations subproject and installation.
    lupdate.sh is from Gens/GS II, but it has been updated to properly
    handle the lack of any *.ts files, and it automatically deletes
    zero-byte *.ts files before running lupdate in order to prevent a
    "premature end of document" error from occurring.
    
    FIXME: A lot of the tr() calls are apparently invalid.
    
    TODO: Install system translations on Win32 and Apple.

---
+++ src/mcrecover/translations/lupdate.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Qt lupdate script for mcrecover.
+# Run this script to update the translations from the source code.
+# NOTE: This script must be run from the translations/ directory!
+
+# Source file extensions.
+SRC_EXTS="h,hpp,c,cpp,m,mm,ui"
+
+# Process the translation files.
+TS_FILES=$(find . -maxdepth 1 -type f -name "*.ts")
+if [[ -z "${TS_FILES}" ]]; then
+	echo "*** ERROR: No translation files (*.ts) were found."
+        echo "*** Create an empty translation file and try again."
+	exit 1
+fi
+
+for TS_FILE in ${TS_FILES}; do
+	if [ ! -s "${TS_FILE}" ]; then
+		rm -f "${TS_FILE}"
+	fi
+	lupdate -extensions "${SRC_EXTS}" ../ -ts "${TS_FILE}"
+done
