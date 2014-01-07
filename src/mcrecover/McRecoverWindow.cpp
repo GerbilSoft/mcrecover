GerbilSoft/mcrecover

commit e1b65a3b62626e3a96fab79a210bc68e3443d43f

Fixed all lupdate errors.
q->tr() is apparently invalid. Use the full context instead,
e.g. McRecoverWindow::tr().

src/mcrecover/McRecoverWindow.cpp
@@ -184,7 +184,7 @@ void McRecoverWindowPrivate::updateLstFileList(void)
 {
 	if (!card) {
 		// Set the group box's title.
-		q->grpFileList->setTitle(q->tr("No memory card loaded."));
+		q->grpFileList->setTitle(McRecoverWindow::tr("No memory card loaded."));
 	} else {
 		// Show the filename.
 		q->grpFileList->setTitle(displayFilename);
@@ -231,7 +231,7 @@ void McRecoverWindowPrivate::initMcToolbar(void)
 
 	// Add a label for the "Preferred region" buttons.
 	// TODO: Add an extra space before the label...
-	QLabel *lblPreferredRegion = new QLabel(q->tr("Preferred region:"));
+	QLabel *lblPreferredRegion = new QLabel(McRecoverWindow::tr("Preferred region:"));
 	q->toolBar->insertWidget(q->actionRegionUS, lblPreferredRegion);
 
 	// Create a QActionGroup for the "Preferred region" buttons.
@@ -327,11 +327,11 @@ void McRecoverWindowPrivate::saveFiles(const QVector<MemCardFile*> files, QStrin
 
 		// Prompt the user for a save location.
 		QString filename = QFileDialog::getSaveFileName(q,
-				q->tr("Save GCN Save File %1")
+				McRecoverWindow::tr("Save GCN Save File %1")
 					.arg(file->filename()),	// Dialog title
 				file->defaultGciFilename(),	// Default filename
-				q->tr("GameCube Save Files") + QLatin1String(" (*.gci);;") +
-				q->tr("All Files") + QLatin1String(" (*)"));
+				McRecoverWindow::tr("GameCube Save Files") + QLatin1String(" (*.gci);;") +
+				McRecoverWindow::tr("All Files") + QLatin1String(" (*)"));
 		if (filename.isEmpty())
 			return;
 
@@ -365,7 +365,7 @@ void McRecoverWindowPrivate::saveFiles(const QVector<MemCardFile*> files, QStrin
 		// Multiple files, path not specified.
 		// Prompt the user for a save location.
 		path = QFileDialog::getExistingDirectory(q,
-				q->tr("Save %Ln GCN Save File(s)", nullptr, files.size()));
+				McRecoverWindow::tr("Save %Ln GCN Save File(s)", nullptr, files.size()));
 		if (path.isEmpty())
 			return;
 	}
@@ -389,13 +389,10 @@ void McRecoverWindowPrivate::saveFiles(const QVector<MemCardFile*> files, QStrin
 			if (overwriteAll == OVERWRITEALL_UNKNOWN) {
 				bool overwrite = false;
 				int ret = QMessageBox::warning(q,
-					q->tr("File Already Exists"),
-					q->tr("A file named \"%1\" already exists in the specified directory.")
-						.arg(filename) +
-					QLatin1String("\n\n") +
-					q->tr("Do you want to overwrite it?"),
-					(QMessageBox::Yes | QMessageBox::No |
-						QMessageBox::YesToAll | QMessageBox::NoToAll),
+					McRecoverWindow::tr("File Already Exists"),
+					McRecoverWindow::tr("A file named \"%1\" already exists in the specified directory.\n\n"
+							    "Do you want to overwrite it?").arg(filename) +
+					(QMessageBox::Yes | QMessageBox::No | QMessageBox::YesToAll | QMessageBox::NoToAll),
 					QMessageBox::No);
 				switch (ret) {
 					case QMessageBox::Yes:

src/mcrecover/AboutDialog.cpp
@@ -373,9 +373,9 @@ QString AboutDialogPrivate::GetCodePageInfo(void)
 
 	cpInfo m_cpInfo[2] = {
 		//: Win32: ANSI code page. (e.g. 1252 for US/English, 932 for Japanese)
-		{CP_ACP,	QT_TR_NOOP("System ANSI code page:")},
+		{CP_ACP,	QT_TRANSLATE_NOOP(AboutDialog, "System ANSI code page:")},
 		//: Win32: OEM code page. (e.g. 437 for US/English)
-		{CP_OEMCP,	QT_TR_NOOP("System OEM code page:")}
+		{CP_OEMCP,	QT_TRANSLATE_NOOP(AboutDialog, "System OEM code page:")}
 	};
 
 	for (int i = 0; i < 2; i++) {
@@ -456,8 +456,8 @@ QString AboutDialogPrivate::GetSupport(void)
 
 	// Support sites.
 	const supportSite_t supportSites[] = {
-		{QT_TR_NOOP("Sonic Retro"), "http://forums.sonicretro.org/index.php?showtopic=31772"},
-		{QT_TR_NOOP("GBAtemp"), "http://gbatemp.net/threads/gcn-memcard-recover.349406/"},
+		{QT_TRANSLATE_NOOP(AboutDialog, "Sonic Retro"), "http://forums.sonicretro.org/index.php?showtopic=31772"},
+		{QT_TRANSLATE_NOOP(AboutDialog, "GBAtemp"), "http://gbatemp.net/threads/gcn-memcard-recover.349406/"},
 		{nullptr, nullptr}
 	};
 

src/mcrecover/MemCardFileView.cpp
@@ -125,7 +125,7 @@ void MemCardFileViewPrivate::updateWidgetDisplay(void)
 	// Is the checksum known?
 	if (file->checksumStatus() == Checksum::CHKST_UNKNOWN) {
 		// Unknown checksum.
-		q->lblChecksumAlgorithm->setText(q->tr("Unknown", "checksum"));
+		q->lblChecksumAlgorithm->setText(MemCardFileView::tr("Unknown", "checksum"));
 		q->lblChecksumActualTitle->setVisible(false);
 		q->lblChecksumActual->setVisible(false);
 		q->lblChecksumExpectedTitle->setVisible(false);
@@ -149,7 +149,7 @@ void MemCardFileViewPrivate::updateWidgetDisplay(void)
 	QVector<QString> checksumValuesFormatted = file->checksumValuesFormatted();
 	if (checksumValuesFormatted.size() < 1) {
 		// No checksum...
-		q->lblChecksumAlgorithm->setText(q->tr("Unknown", "checksum"));
+		q->lblChecksumAlgorithm->setText(MemCardFileView::tr("Unknown", "checksum"));
 		q->lblChecksumActualTitle->setVisible(false);
 		q->lblChecksumActual->setVisible(false);
 		q->lblChecksumExpectedTitle->setVisible(false);

src/mcrecover/MemCardView.cpp
@@ -98,7 +98,7 @@ void MemCardViewPrivate::updateWidgetDisplay(void)
 	vector<string> checksumValuesFormatted = Checksum::ChecksumValuesFormatted(checksumValues);
 	if (checksumValuesFormatted.size() < 1) {
 		// No checksum...
-		q->lblChecksumActual->setText(q->tr("Unknown", "checksum"));
+		q->lblChecksumActual->setText(MemCardView::tr("Unknown", "checksum"));
 		q->lblChecksumExpectedTitle->setVisible(false);
 		q->lblChecksumExpected->setVisible(false);
 	} else {
@@ -133,7 +133,7 @@ void MemCardViewPrivate::updateWidgetDisplay(void)
 	}
 
 	// Block count.
-	q->lblBlockCount->setText(q->tr("%L1 block(s) (%L2 free)")
+	q->lblBlockCount->setText(MemCardView::tr("%L1 block(s) (%L2 free)")
 				.arg(card->sizeInBlocksNoSys())
 				.arg(card->freeBlocks()));
 
@@ -146,7 +146,7 @@ void MemCardViewPrivate::updateWidgetDisplay(void)
 		QIcon icon = McRecoverQApplication::IconFromTheme(QLatin1String("dialog-error"));
 		// TODO: What size?
 		q->lblStatusIcon->setPixmap(icon.pixmap(16, 16));
-		q->lblStatusIcon->setToolTip(q->tr("Memory card header is corrupted."));
+		q->lblStatusIcon->setToolTip(MemCardView::tr("Memory card header is corrupted."));
 		q->lblStatusIcon->setVisible(true);
 	}
 

src/mcrecover/StatusBarManager.cpp
@@ -106,7 +106,7 @@ StatusBarManagerPrivate::StatusBarManagerPrivate(StatusBarManager *q)
 	, taskbarButtonManager(TaskbarButtonManager::Instance(q))
 {
 	// Default message.
-	lastStatusMessage = q->tr("Ready.");
+	lastStatusMessage = StatusBarManager::tr("Ready.");
 
 	// Initialize the timer.
 	tmrHideProgressBar.setInterval(SECONDS_TO_HIDE_PROGRESS_BAR * 1000);
@@ -131,14 +131,13 @@ void StatusBarManagerPrivate::updateStatusBar(void)
 {
 	if (scanning) {
 		// We're scanning for files.
-		lastStatusMessage = q->tr("Scanning block #%L1 (%L2 scanned, %L3 remaining)...")
+		lastStatusMessage = StatusBarManager::tr("Scanning block #%L1 (%L2 scanned, %L3 remaining)...")
 					.arg(currentPhysBlock)
 					.arg(currentSearchBlock)
 					.arg(totalSearchBlocks - currentSearchBlock);
 
-		// TODO: Show number of files found?
-		/*
-		QString filesFoundText = q->tr("%n lost file(s) found.", nullptr, lostFilesFound);
+		/* TODO: Show number of files found?
+		QString filesFoundText = StatusBarManager::tr("%n lost file(s) found.", nullptr, lostFilesFound);
 		q->lblFilesFound->setText(filesFoundText);
 		*/
 

commit fd3536a983c4149abafc763e70418b44b6908bc1

McRecoverWindowPrivate::saveFiles(): Fixed a build error.
This is a regression from commit e1b65a3b62626e3a96fab79a210bc68e3443d43f.
(Fixed all lupdate errors.)

src/mcrecover/McRecoverWindow.cpp
@@ -391,7 +391,7 @@ void McRecoverWindowPrivate::saveFiles(const QVector<MemCardFile*> files, QStrin
 				int ret = QMessageBox::warning(q,
 					McRecoverWindow::tr("File Already Exists"),
 					McRecoverWindow::tr("A file named \"%1\" already exists in the specified directory.\n\n"
-							    "Do you want to overwrite it?").arg(filename) +
+							    "Do you want to overwrite it?").arg(filename),
 					(QMessageBox::Yes | QMessageBox::No | QMessageBox::YesToAll | QMessageBox::NoToAll),
 					QMessageBox::No);
 				switch (ret) {