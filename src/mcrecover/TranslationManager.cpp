c9d296b00c14f66ee7c4670d60ec962e2bf08c25
David Korth <gerbilsoft@gerbilsoft.com>
2014-01-08 21:14:45 -0500

    TranslationManager::enumerate(): nameFilers_c[] has 4 entries, not 8.
    This was a copy/paste error from GcnMcFileDb::GetDbFilenames().
    
    Found using cppcheck-1.63.1.

--- src/mcrecover/TranslationManager.cpp
+++ src/mcrecover/TranslationManager.cpp
@@ -196,8 +196,8 @@ QMap<QString, QString> TranslationManager::enumerate(void) const
 	};
 
 	QStringList nameFilters;
-	nameFilters.reserve(8);
-	for (int i = 0; i < 8; i++)
+	nameFilters.reserve(4);
+	for (int i = 0; i < 4; i++)
 		nameFilters << QLatin1String(nameFilters_c[i]);
 
 	// Search the paths for TS files.
