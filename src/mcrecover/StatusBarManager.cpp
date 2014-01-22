GerbilSoft/mcrecover

commit 5c3ebdd56c8d00b28ffa8d5050c31f83bdc2ed80

[mcrecover] Use const refs for Qt objects.
While Qt objects are implicitly shared, passing them by value
still requires adjusting the object's reference counter.

Passing by const ref removes this adjustment.

src/mcrecover/StatusBarManager.cpp
@@ -339,17 +339,19 @@ void StatusBarManager::setSearchThread(SearchThread *searchThread)
  * A GameCube Memory Card image was opened.
  * @param filename Filename.
  */
-void StatusBarManager::opened(QString filename)
+void StatusBarManager::opened(const QString &filename)
 {
 	// Extract the filename from the path.
 	int lastSlash = filename.lastIndexOf(QChar(L'/'));
-	if (lastSlash >= 0)
-		filename.remove(0, lastSlash + 1);
+	QStringRef filenameRef = (lastSlash >= 0
+			? QStringRef(&filename, 0, lastSlash+1)
+			: QStringRef(&filename));
 
 	Q_D(StatusBarManager);
 	d->scanning = false;
 	d->progressBar->setVisible(false);
-	d->lastStatusMessage = tr("Loaded GameCube Memory Card image %1").arg(filename);
+	d->lastStatusMessage = tr("Loaded GameCube Memory Card image %1")
+				.arg(filenameRef.toString());
 	d->updateStatusBar();
 
 	// Stop the Hide Progress Bar timer.
@@ -376,7 +378,7 @@ void StatusBarManager::closed(void)
  * @param n Number of files saved.
  * @param path Path files were saved to.
  */
-void StatusBarManager::filesSaved(int n, QString path)
+void StatusBarManager::filesSaved(int n, const QString &path)
 {
 	Q_D(StatusBarManager);
 	d->scanning = false;

src/mcrecover/McRecoverWindow.cpp
@@ -774,7 +774,7 @@ McRecoverWindow::~McRecoverWindow()
  * Open a GameCube Memory Card image.
  * @param filename Filename.
  */
-void McRecoverWindow::openCard(QString filename)
+void McRecoverWindow::openCard(const QString &filename)
 {
 	Q_D(McRecoverWindow);
 

src/mcrecover/McRecoverWindow.hpp
@@ -52,7 +52,7 @@ class McRecoverWindow : public QMainWindow
 		 * Open a GameCube Memory Card image.
 		 * @param filename Filename.
 		 */
-		void openCard(QString filename);
+		void openCard(const QString &filename);
 
 		/**
 		 * Close the currently-opened GameCube Memory Card image.

src/mcrecover/MemCard.cpp
@@ -45,7 +45,7 @@
 class MemCardPrivate
 {
 	public:
-		MemCardPrivate(MemCard *q, QString filename);
+		MemCardPrivate(MemCard *q, const QString &filename);
 		~MemCardPrivate();
 		void init(void);
 
@@ -138,8 +138,9 @@ class MemCardPrivate
 QTextCodec *MemCardPrivate::TextCodecUS = nullptr;	// cp1252
 QTextCodec *MemCardPrivate::TextCodecJP = nullptr;	// Shift-JIS
 
-MemCardPrivate::MemCardPrivate(MemCard *q, QString filename)
+MemCardPrivate::MemCardPrivate(MemCard *q, const QString &filename)
 	: q_ptr(q)
+	, filename(filename)
 	, file(nullptr)
 	, mc_dat(nullptr)
 	, mc_bat(nullptr)
@@ -149,11 +150,8 @@ MemCardPrivate::MemCardPrivate(MemCard *q, QString filename)
 
 	// TODO: Set an error code somewhere.
 
-	// Save the filename.
-	this->filename = filename;
 	if (filename.isEmpty()) {
 		// No filename specified.
-		file = nullptr;
 		return;
 	}
 
@@ -772,7 +770,7 @@ MemCardFile *MemCard::addLostFile(const card_direntry *dirEntry)
  * @param fatEntries FAT entries.
  * @return MemCardFile added to the MemCard, or nullptr on error.
  */
-MemCardFile *MemCard::addLostFile(const card_direntry *dirEntry, QVector<uint16_t> fatEntries)
+MemCardFile *MemCard::addLostFile(const card_direntry *dirEntry, const QVector<uint16_t> &fatEntries)
 {
 	if (!isOpen())
 		return nullptr;

src/mcrecover/MemCard.hpp
@@ -204,7 +204,7 @@ class MemCard : public QObject
 		 * @param fatEntries FAT entries.
 		 * @return MemCardFile added to the MemCard, or nullptr on error.
 		 */
-		MemCardFile *addLostFile(const card_direntry *dirEntry, QVector<uint16_t> fatEntries);
+		MemCardFile *addLostFile(const card_direntry *dirEntry, const QVector<uint16_t> &fatEntries);
 
 		/**
 		 * Get the header checksum value.

src/mcrecover/MemCardFile.cpp
@@ -71,7 +71,7 @@ class MemCardFilePrivate
 		MemCardFilePrivate(MemCardFile *q,
 				MemCard *card,
 				const card_direntry *dirEntry,
-				QVector<uint16_t> fatEntries);
+				const QVector<uint16_t> &fatEntries);
 
 		~MemCardFilePrivate();
 
@@ -258,7 +258,7 @@ MemCardFilePrivate::MemCardFilePrivate(MemCardFile *q,
  */
 MemCardFilePrivate::MemCardFilePrivate(MemCardFile *q,
 		MemCard *card, const card_direntry *dirEntry,
-		QVector<uint16_t> fatEntries)
+		const QVector<uint16_t> &fatEntries)
 	: q_ptr(q)
 	, card(card)
 	, fileIdx(-1)
@@ -875,7 +875,7 @@ MemCardFile::MemCardFile(MemCard *card, const int fileIdx,
  */
 MemCardFile::MemCardFile(MemCard *card,
 		const card_direntry *dirEntry,
-		QVector<uint16_t> fatEntries)
+		const QVector<uint16_t> &fatEntries)
 	: QObject(card)
 	, d_ptr(new MemCardFilePrivate(this, card, dirEntry, fatEntries))
 { }
@@ -1099,7 +1099,7 @@ QVector<Checksum::ChecksumDef> MemCardFile::checksumDefs(void) const
  * Set the checksum definitions.
  * @param checksumDefs Checksum definitions.
  */
-void MemCardFile::setChecksumDefs(QVector<Checksum::ChecksumDef> checksumDefs)
+void MemCardFile::setChecksumDefs(const QVector<Checksum::ChecksumDef> &checksumDefs)
 {
 	Q_D(MemCardFile);
 	d->checksumDefs = checksumDefs;
@@ -1195,7 +1195,7 @@ QString MemCardFile::defaultGciFilename(void) const
  * @return 0 on success; non-zero on error.
  * TODO: Error code constants.
  */
-int MemCardFile::saveGci(QString filename)
+int MemCardFile::saveGci(const QString &filename)
 {
 	QFile file(filename);
 	if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {

src/mcrecover/MemCardFile.hpp
@@ -96,7 +96,7 @@ class MemCardFile : public QObject
 		 */
 		MemCardFile(MemCard *card,
 				const card_direntry *dirEntry,
-				QVector<uint16_t> fatEntries);
+				const QVector<uint16_t> &fatEntries);
 
 		~MemCardFile();
 	
@@ -230,7 +230,7 @@ class MemCardFile : public QObject
 		 * Set the checksum definitions.
 		 * @param checksumDefs Checksum definitions.
 		 */
-		void setChecksumDefs(QVector<Checksum::ChecksumDef> checksumDefs);
+		void setChecksumDefs(const QVector<Checksum::ChecksumDef> &checksumDefs);
 
 		/**
 		 * Get the checksum values.
@@ -271,7 +271,7 @@ class MemCardFile : public QObject
 		 * @return 0 on success; non-zero on error.
 		 * TODO: Error code constants.
 		 */
-		int saveGci(QString filename);
+		int saveGci(const QString &filename);
 
 		/**
 		 * Save the file in GCI format.

src/mcrecover/PcreRegex.cpp
@@ -104,7 +104,7 @@ int PcreRegex::setRegex(const QString &regex, int *errOffset)
  * @param outVector	[out, opt] Output vector for substring matches.
  * @return Number of substring matches on success, or PCRE_ERROR_* value (negative number) on error.
  */
-int PcreRegex::exec(QByteArray subjectUtf8, QVector<QString> *outVector) const
+int PcreRegex::exec(const QByteArray &subjectUtf8, QVector<QString> *outVector) const
 {
 	if (!m_regex)
 		return PCRE_ERROR_NOMATCH;

commit b3103b467d155830d4a8ded482872f68a414087f

[mcrecover] StatusBarManager::opened(): Extract the filename, not the path.
The changes to cope with const QString& ended up extracting the
directory, not the filename.

Use a QFileInfo instead of manual QString operations to ensure
that we get it right.

This is a regression from commit 5c3ebdd56c8d00b28ffa8d5050c31f83bdc2ed80.
([mcrecover] Use const refs for Qt objects.)

src/mcrecover/StatusBarManager.cpp
@@ -341,16 +341,14 @@ void StatusBarManager::setSearchThread(SearchThread *searchThread)
 void StatusBarManager::opened(const QString &filename)
 {
 	// Extract the filename from the path.
-	int lastSlash = filename.lastIndexOf(QChar(L'/'));
-	QStringRef filenameRef = (lastSlash >= 0
-			? QStringRef(&filename, 0, lastSlash+1)
-			: QStringRef(&filename));
+	QFileInfo fileInfo(filename);
+	QString filename_noPath = fileInfo.fileName();
 
 	Q_D(StatusBarManager);
 	d->scanning = false;
 	d->progressBar->setVisible(false);
 	d->lastStatusMessage = tr("Loaded GameCube Memory Card image %1")
-				.arg(filenameRef.toString());
+				.arg(filename_noPath);
 	d->updateStatusBar();
 
 	// Stop the Hide Progress Bar timer.