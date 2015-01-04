e03f25d00f6445487a66e5fe3f39191e84cae1cf
David Korth <gerbilsoft@gerbilsoft.com>
2015-01-04 13:02:32 -0500

    [mcrecover] AboutDialog: Updated copyright date to 2015.

--- src/mcrecover/windows/AboutDialog.cpp
+++ src/mcrecover/windows/AboutDialog.cpp
@@ -2,7 +2,7 @@
  * GameCube Memory Card Recovery Program.                                  *
  * AboutDialog.cpp: About Dialog.                                          *
  *                                                                         *
- * Copyright (c) 2013-2014 by David Korth.                                 *
+ * Copyright (c) 2013-2015 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -217,7 +217,7 @@ QString AboutDialogPrivate::GetCredits(void)
 
 	QString credits;
 	credits.reserve(4096);
-	credits += QLatin1String("Copyright (c) 2012-2014 by David Korth.");
+	credits += QLatin1String("Copyright (c) 2012-2015 by David Korth.");
 
 	enum CreditType_t {
 		CT_CONTINUE = 0,	// Continue previous type.
