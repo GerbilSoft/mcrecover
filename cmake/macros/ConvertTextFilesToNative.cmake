GerbilSoft/mcrecover

commit 079d51e3febac3fbfc833eba42112b99303a6d67

ConvertTextFilesToNative.cmake: Disable if compiling with MSVC.
`unix2dos' isn't working correctly with MSVC. Also, if the user
is compiling with MSVC, the repository was probably checked out
with automatic CRLF conversions in the first place, so the files
don't need to be converted a second time.

src/CMakeLists.txt: Don't build `miniu2d' if compiling with MSVC,
since it won't be used.

cmake/macros/ConvertTextFilesToNative.cmake
@@ -2,7 +2,7 @@
 # This only has an effect on Win32 platforms, since the software is developed
 # on Linux and hence uses Unix line endings.
 
-IF(WIN32)
+IF(WIN32 AND NOT MSVC)
 	# Search for unix2dos.
 	FIND_PROGRAM(UNIX2DOS unix2dos)
 ENDIF(WIN32)
@@ -11,7 +11,7 @@ ENDIF(WIN32)
 # - _filenames: Variable to store converted (or as-is) filenames in.
 # - Additional: Files to convert.
 MACRO(CONVERT_TEXT_FILES_TO_NATIVE _filenames)
-	IF(WIN32)
+	IF(WIN32 AND NOT MSVC)
 		# Win32: Convert text files to use Windows line endings.
 		IF(NOT UNIX2DOS)
 			GET_PROPERTY(MINIU2D_EXE_LOCATION TARGET miniu2d PROPERTY LOCATION)
@@ -24,6 +24,7 @@ MACRO(CONVERT_TEXT_FILES_TO_NATIVE _filenames)
 
 			IF(UNIX2DOS)
 				# Use unix2dos.
+				# FIXME: Not working with nmake...
 				ADD_CUSTOM_COMMAND(
 					OUTPUT "${CURRENT_OUTPUT_FILE}"
 					COMMAND ${UNIX2DOS} <"${CURRENT_SOURCE_FILE}" >"${CURRENT_OUTPUT_FILE}"

src/CMakeLists.txt
@@ -44,9 +44,9 @@ ADD_SUBDIRECTORY(libgctools)
 ADD_SUBDIRECTORY(mcrecover)
 ADD_SUBDIRECTORY(gcbanner)
 
-IF(WIN32)
+IF(WIN32 AND NOT MSVC)
 	FIND_PROGRAM(UNIX2DOS unix2dos)
 	IF(NOT UNIX2DOS)
 		ADD_SUBDIRECTORY(miniu2d)
 	ENDIF(NOT UNIX2DOS)
-ENDIF(WIN32)
+ENDIF(WIN32 AND NOT MSVC)

commit ee17136f878ff7dc0851229f218691df2dfeeaa0

ConvertTextFilesToNative.cmake: Fix ELSE() and ENDIF() blocks.
I forgot to update these in the previous commit.

This is a regression from commit 079d51e3febac3fbfc833eba42112b99303a6d67.
(ConvertTextFilesToNative.cmake: Disable if compiling with MSVC.)

cmake/macros/ConvertTextFilesToNative.cmake
@@ -5,7 +5,7 @@
 IF(WIN32 AND NOT MSVC)
 	# Search for unix2dos.
 	FIND_PROGRAM(UNIX2DOS unix2dos)
-ENDIF(WIN32)
+ENDIF(WIN32 AND NOT MSVC)
 
 # Parameters:
 # - _filenames: Variable to store converted (or as-is) filenames in.
@@ -55,8 +55,8 @@ MACRO(CONVERT_TEXT_FILES_TO_NATIVE _filenames)
 		IF(NOT UNIX2DOS)
 			UNSET(MINIU2D_EXE_LOCATION)
 		ENDIF(NOT UNIX2DOS)
-	ELSE(WIN32)
-		# Not Win32. Don't do anything.
+	ELSE(WIN32 AND NOT MSVC)
+		# Not Win32, or compiling with MSVC. Don't do anything.
 		SET(${_filenames} ${ARGN})
-	ENDIF(WIN32)
+	ENDIF(WIN32 AND NOT MSVC)
 ENDMACRO(CONVERT_TEXT_FILES_TO_NATIVE)