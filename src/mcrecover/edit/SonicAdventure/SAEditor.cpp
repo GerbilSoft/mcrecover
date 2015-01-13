6232bf4634a189eea0e45a3673a3c45d415c13f0
David Korth <gerbilsoft@gerbilsoft.com>
2015-01-12 20:38:50 -0500

    SAEditor: Load all save slots for DC files.
    The save slot can't be selected yet, but this is a start
    for multiple slot editing.
    
    d->data[]: QVector containing allocated sa_save_file* data.
    This is used for single-slot (GCN) and multi-slot (Dreamcast)
    files. Note that since sa_save_file is a C struct, the data
    is allocated using malloc() and free().
    
    d->updateDisplay(): New function for updating the display.

--- src/mcrecover/edit/SonicAdventure/SAEditor.cpp
+++ src/mcrecover/edit/SonicAdventure/SAEditor.cpp
@@ -21,6 +21,9 @@
 
 #include "SAEditor.hpp"
 
+// C includes. (C++ namespace)
+#include <cstdlib>
+
 // Qt includes.
 #include <QtCore/QEvent>
 
@@ -40,6 +43,7 @@ class SAEditorPrivate
 {
 	public:
 		SAEditorPrivate(SAEditor *q);
+		~SAEditorPrivate();
 
 	protected:
 		SAEditor *const q_ptr;
@@ -49,11 +53,69 @@ class SAEditorPrivate
 
 	public:
 		Ui::SAEditor ui;
+
+		// File being edited.
+		// TODO: EditorManager to handle File being destroyed.
+		File *file;
+
+		// sa_save_file structs.
+		QVector<sa_save_file*> data;
+		int slot; // active slot (-1 for none)
+
+		/**
+		 * Clear the sa_save_file structs.
+		 */
+		void clearData(void);
+
+		/**
+		 * Update the display.
+		 */
+		void updateDisplay(void);
 };
 
 SAEditorPrivate::SAEditorPrivate(SAEditor* q)
 	: q_ptr(q)
+	, slot(-1)
 { }
+
+SAEditorPrivate::~SAEditorPrivate()
+{
+	clearData();
+}
+
+/**
+ * Clear the sa_save_file structs.
+ */
+void SAEditorPrivate::clearData(void)
+{
+	// Delete all loaded sa_save structs.
+	// NOTE: sa_save_file is a C struct, so use free().
+	foreach (sa_save_file *sa_save, data) {
+		free(sa_save);
+	}
+	data.clear();
+}
+
+/**
+ * Update the display.
+ */
+void SAEditorPrivate::updateDisplay(void)
+{
+	if (slot < 0)
+		slot = 0;
+	if (slot >= data.size())
+		slot = (data.size() - 1);
+
+	if (slot < 0 || slot >= data.size()) {
+		// Invalid slot number.
+		// TODO: Clear the displays.
+		return;
+	}
+
+	// Display the data.
+	sa_save_file *sa_save = data.at(slot);
+	ui.saLevelStats->loadSaveData(sa_save);
+}
 
 /** SAEditor **/
 
@@ -102,35 +164,42 @@ void SAEditor::changeEvent(QEvent *event)
 void SAEditor::setFile(File *file)
 {
 	Q_D(SAEditor);
+	d->clearData();
+	d->file = file;
 
 	QByteArray data = file->loadFileData();
-	sa_save_file *sa_save;
 	if (file->filename() == QLatin1String("SONICADV_SYS") ||
 	    file->filename() == QLatin1String("SONICADV_INT"))
 	{
 		// DC version.
 		// TODO: Verify that this is an SA1 file.
 		// TODO: Show a slot selector.
-		if (data.size() < (SA_SAVE_ADDRESS_DC_0 + SA_SAVE_FILE_LEN)) {
+		if (data.size() < (SA_SAVE_ADDRESS_DC_0 + (SA_SAVE_FILE_LEN * 3))) {
 			// TODO: Show an error.
 			return;
 		}
 
-		sa_save = (sa_save_file*)(data.data() + SA_SAVE_ADDRESS_DC_0);
+		// Three, count 'em, *three* save slots!
+		const char *src = (data.data() + SA_SAVE_ADDRESS_DC_0);
+		for (int i = 0; i < 3; i++, src += SA_SAVE_FILE_LEN) {
+			sa_save_file *sa_save = (sa_save_file*)malloc(sizeof(*sa_save));
+			memcpy(sa_save, src, SA_SAVE_FILE_LEN);
+			d->data.append(sa_save);
 
 #if MCRECOVER_BYTEORDER == MCRECOVER_BIG_ENDIAN
-		// Byteswap the data.
-		// Dreamcast's SH-4 is little-endian.
-		for (int i = 0; i < NUM_ELEMENTS(sa_save->scores.all); i++) {
-			sa_save->scores.all[i] = le32_to_cpu(sa_save->scores.all[i]);
-		}
-		for (int i = 0; i < NUM_ELEMENTS(sa_save->weights.all); i++) {
-			sa_save->weights.all[i] = le16_to_cpu(sa_save->weights.all[i]);
-		}
-		for (int i = 0; i < NUM_ELEMENTS(sa_save->rings.all); i++) {
-			sa_save->rings.all[i] = le16_to_cpu(sa_save->rings.all[i]);
-		}
+			// Byteswap the data.
+			// Dreamcast's SH-4 is little-endian.
+			for (int i = 0; i < NUM_ELEMENTS(sa_save->scores.all); i++) {
+				sa_save->scores.all[i] = le32_to_cpu(sa_save->scores.all[i]);
+			}
+			for (int i = 0; i < NUM_ELEMENTS(sa_save->weights.all); i++) {
+				sa_save->weights.all[i] = le16_to_cpu(sa_save->weights.all[i]);
+			}
+			for (int i = 0; i < NUM_ELEMENTS(sa_save->rings.all); i++) {
+				sa_save->rings.all[i] = le16_to_cpu(sa_save->rings.all[i]);
+			}
 #endif
+		}
 	} else if (file->filename().startsWith(QLatin1String("SONICADVENTURE_DX_PLAYRECORD_"))) {
 		// GameCube verison.
 		// TODO: Verify that this is an SADX file.
@@ -139,7 +208,10 @@ void SAEditor::setFile(File *file)
 			return;
 		}
 
-		sa_save = (sa_save_file*)(data.data() + SA_SAVE_ADDRESS_GCN);
+		// Only one save slot.
+		sa_save_file *sa_save = (sa_save_file*)malloc(sizeof(*sa_save));
+		memcpy(sa_save, (data.data() + SA_SAVE_ADDRESS_GCN), SA_SAVE_FILE_LEN);
+		d->data.append(sa_save);
 
 #if MCRECOVER_BYTEORDER == MCRECOVER_LIL_ENDIAN
 		// Byteswap the data.
@@ -157,10 +229,12 @@ void SAEditor::setFile(File *file)
 	} else {
 		// Unsupported file.
 		// TODO: Show an error.
+		d->file = nullptr;
 		return;
 	}
 
-	// TODO: Save the sa_save internally.
-	// Show the save data.
-	d->ui.saLevelStats->loadSaveData(sa_save);
+	// Update the display.
+	// TODO: Slot selection.
+	d->slot = 0;
+	d->updateDisplay();
 }
