GerbilSoft/mcrecover

commit a4c439b6106fdc7e1d365ae96f36387ec2190bdc

MemCardModel::setCard(): Don't set d->fileCount until we're done manipulating the data.

src/mcrecover/card/MemCardModel.cpp
@@ -518,9 +518,9 @@ void MemCardModel::setCard(Card *card)
 	if (d->card) {
 		// Notify the view that we're about to remove all rows.
 		// TODO: fileCount should already be cached...
-		d->fileCount = d->card->fileCount();
-		if (d->fileCount > 0)
-			beginRemoveRows(QModelIndex(), 0, (d->fileCount - 1));
+		const int fileCount = d->card->fileCount();
+		if (fileCount > 0)
+			beginRemoveRows(QModelIndex(), 0, (fileCount - 1));
 
 		// Disconnect the Card's signals.
 		disconnect(d->card, SIGNAL(destroyed(QObject*)),
@@ -537,17 +537,16 @@ void MemCardModel::setCard(Card *card)
 		d->card = nullptr;
 
 		// Done removing rows.
-		if (d->fileCount > 0) {
-			d->fileCount = 0;
+		d->fileCount = 0;
+		if (fileCount > 0)
 			endRemoveRows();
-		}
 	}
 
 	if (card) {
 		// Notify the view that we're about to add rows.
-		d->fileCount = card->fileCount();
-		if (d->fileCount > 0)
-			beginInsertRows(QModelIndex(), 0, (d->fileCount - 1));
+		const int fileCount = card->fileCount();
+		if (fileCount > 0)
+			beginInsertRows(QModelIndex(), 0, (fileCount - 1));
 
 		// Set the card.
 		d->card = card;
@@ -568,8 +567,10 @@ void MemCardModel::setCard(Card *card)
 			this, SLOT(card_filesRemoved_slot()));
 
 		// Done adding rows.
-		if (d->fileCount > 0)
+		if (fileCount > 0) {
+			d->fileCount = fileCount;
 			endInsertRows();
+		}
 	}
 }
 

commit 8a7448abd2f925375ffab78cd6714b325b9299ba

MemCardModel::setCard(): Animated icons stopped working.
d->fileCount has to be set before calling d->initAnimState().

This is a regression from commit a4c439b6106fdc7e1d365ae96f36387ec2190bdc.
(MemCardModel::setCard(): Don't set d->fileCount until we're done manipulating the data.)

src/mcrecover/card/MemCardModel.cpp
@@ -552,6 +552,8 @@ void MemCardModel::setCard(Card *card)
 		d->card = card;
 
 		// Initialize the animation state.
+		// NOTE: fileCount must be set here.
+		d->fileCount = fileCount;
 		d->initAnimState();
 
 		// Connect the Card's signals.
@@ -567,10 +569,8 @@ void MemCardModel::setCard(Card *card)
 			this, SLOT(card_filesRemoved_slot()));
 
 		// Done adding rows.
-		if (fileCount > 0) {
-			d->fileCount = fileCount;
+		if (fileCount > 0)
 			endInsertRows();
-		}
 	}
 }
 