4d3b6c915634ac962cd56d73554cead18edda109
David Korth <gerbilsoft@gerbilsoft.com>
2016-01-11 00:28:15 -0500

    [SonicAdventure] Updated the copyright dates for SAEditor and SAGeneral.

--- src/mcrecover/edit/SonicAdventure/SAEditor.cpp
+++ src/mcrecover/edit/SonicAdventure/SAEditor.cpp
@@ -2,7 +2,7 @@
  * GameCube Memory Card Recovery Program.                                  *
  * SAEditor.cpp: Sonic Adventure - save file editor.                       *
  *                                                                         *
- * Copyright (c) 2015 by David Korth.                                      *
+ * Copyright (c) 2015-2016 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
