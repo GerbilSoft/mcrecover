GerbilSoft/mcrecover

commit 1cd03b4ad3ad3143b4471736971675dc4f61e5e8

[SonicAdventure] SALevelStats: Cache the QString level names.
This probably won't decrease memory usage significantly, but it will
improve performance because it won't have to keep converting the
strings from C strings.

src/mcrecover/edit/SonicAdventure/SALevelStats.cpp
@@ -89,6 +89,9 @@ class SALevelStatsPrivate
 		// Current character being displayed.
 		int character;
 
+		// Cached level names.
+		QString actionStageNameCache[SA_LEVEL_NAMES_ACTION_COUNT];
+
 		/**
 		 * Clear the loaded data.
 		 * This does NOT automatically update the UI.
@@ -186,6 +189,11 @@ SALevelStatsPrivate::SALevelStatsPrivate(SALevelStats *q)
 {
 	// Clear the data.
 	clear();
+
+	// Cache level names as QStrings.
+	for (int i = 0; i < NUM_ELEMENTS(actionStageNameCache); i++) {
+		actionStageNameCache[i] = QLatin1String(sa_level_names_action[i]);
+	}
 }
 
 SALevelStatsPrivate::~SALevelStatsPrivate()
@@ -310,7 +318,7 @@ void SALevelStatsPrivate::switchLevels(int character)
 	int i = 0;
 	for (; i < MAX_LEVELS && *levelID != -1; i++, levelID++) {
 		// TODO: Cache the level name QStrings.
-		levels[i].lblLevel->setText(QLatin1String(sa_level_names_action[*levelID]));
+		levels[i].lblLevel->setText(actionStageNameCache[i]);
 		levels[i].lblLevel->show();
 		levels[i].spnHighScore->show();
 		levels[i].spnMostRings->show();

commit e22e91f30788cdd0fddeadd17018106dbb6a84d5

[SonicAdventure] SALevelStatsPrivate::switchLevels(): Use the level map ID for the level name.
This fixes ordering for all characters other than Sonic, since it was
using the row ID for the level name instead of the mapped level ID.

This is a regression from commit 1cd03b4ad3ad3143b4471736971675dc4f61e5e8.
([SonicAdventure] SALevelStats: Cache the QString level names.)

src/mcrecover/edit/SonicAdventure/SALevelStats.cpp
@@ -312,7 +312,7 @@ void SALevelStatsPrivate::switchLevels(int character)
 	int i = 0;
 	for (; i < MAX_LEVELS && *levelID != -1; i++, levelID++) {
 		// TODO: Cache the level name QStrings.
-		levels[i].lblLevel->setText(actionStageNameCache[i]);
+		levels[i].lblLevel->setText(actionStageNameCache[*levelID]);
 		levels[i].lblLevel->show();
 		levels[i].spnHighScore->show();
 		levels[i].spnMostRings->show();