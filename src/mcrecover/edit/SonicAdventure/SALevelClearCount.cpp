GerbilSoft/mcrecover

commit 3568c1066d8696680c6862a9122b819575de6f52

[SonicAdventure] Some more minor improvements.
Converted some 'for' loops from ascending to descending.

SASubGames: Moved two setAlignment() calls out of the code and
into the Qt Designer file.

src/mcrecover/edit/SonicAdventure/SALevelClearCount.cpp
@@ -152,9 +152,9 @@ SALevelClearCountPrivate::~SALevelClearCountPrivate()
 	// TODO: Is this needed?
 	// The level widgets are owned by this widget, so they
 	// should be automatically deleted...
-	for (int i = 0; i < TOTAL_LEVELS; i++) {
+	for (int i = TOTAL_LEVELS-1; i >= 0; i--) {
 		delete levels[i].lblLevel;
-		for (int j = 0; j < NUM_ELEMENTS(levels[i].spnCount); j++) {
+		for (int j = NUM_ELEMENTS(levels[i].spnCount)-1; j >= 0; j--) {
 			delete levels[i].spnCount[j];
 		}
 	}
@@ -258,8 +258,8 @@ void SALevelClearCountPrivate::updateDisplay(void)
 			    q, SLOT(spnCount_mapped_slot(int)));
 
 	// NOTE: Outer loop is the character due to cache locality.
-	for (int chr = 0; chr < NUM_ELEMENTS(levels[0].spnCount); chr++) {
-		for (int level = 0; level < NUM_ELEMENTS(levels); level++) {
+	for (int chr = NUM_ELEMENTS(levels[0].spnCount; chr >= 0); chr--) {
+		for (int level = NUM_ELEMENTS(levels); level >= 0; level--) {
 			levels[level].spnCount[chr]->setValue(clear_count.all[chr][level]);
 		}
 	}

src/mcrecover/edit/SonicAdventure/SAMiscEmblems.cpp
@@ -80,10 +80,10 @@ SAMiscEmblemsPrivate::~SAMiscEmblemsPrivate()
 	// TODO: Is this needed?
 	// The widgets are owned by this widget, so they
 	// should be automatically deleted...
-	for (int i = 0; i < NUM_ELEMENTS(chkChaoRace); i++) {
+	for (int i = NUM_ELEMENTS(chkChaoRace); i >= 0; i--) {
 		delete chkChaoRace[i];
 	}
-	for (int i = 0; i < NUM_ELEMENTS(chkAdvField); i++) {
+	for (int i = NUM_ELEMENTS(chkAdvField); i >= 0; i--) {
 		delete chkAdvField[i];
 	}
 }

src/mcrecover/edit/SonicAdventure/SASubGames.cpp
@@ -466,12 +466,8 @@ SASubGames::SASubGames(QWidget *parent)
 	d->ui.chkTwinkleCircuit_1->setStyleSheet(qsCssCheckBox);
 	d->ui.chkTwinkleCircuit_2->setStyleSheet(qsCssCheckBox);
 
-	// Center-align the Twinkle Circuit emblem checkboxes.
-	// TODO: More alignment.
 	// TODO: Some of the Time boxes are so wide they no longer look like times.
 	// (Added horizontal spacers to fix some of them...)
-	d->ui.hboxTwinkleCircuitEmblems->setAlignment(d->ui.chkTwinkleCircuit_1, Qt::AlignHCenter);
-	d->ui.hboxTwinkleCircuitEmblems->setAlignment(d->ui.chkTwinkleCircuit_2, Qt::AlignHCenter);
 
 	// Twinkle Circuit uses centiseconds.
 	d->ui.tceTwinkleCircuitBestTimes_1->setDisplayMode(TimeCodeEdit::DM_MSC);

src/mcrecover/edit/SonicAdventure/SASubGames.ui
@@ -440,10 +440,10 @@
                </property>
               </spacer>
              </item>
-             <item>
+             <item alignment="Qt::AlignHCenter">
               <widget class="QCheckBox" name="chkTwinkleCircuit_1"/>
              </item>
-             <item>
+             <item alignment="Qt::AlignHCenter">
               <widget class="QCheckBox" name="chkTwinkleCircuit_2"/>
              </item>
              <item>

commit ef62683353c99d9fb3598df92800ebd5f6e4550b

[SonicAdventure] SALevelClearCountPrivate::updateDisplay(): Fixed a dumb typo.
Parenthesis goes where?

This is a regression from commit 3568c1066d8696680c6862a9122b819575de6f52.
([SonicAdventure] Some more minor improvements.)

src/mcrecover/edit/SonicAdventure/SALevelClearCount.cpp
@@ -258,7 +258,7 @@ void SALevelClearCountPrivate::updateDisplay(void)
 			    q, SLOT(spnCount_mapped_slot(int)));
 
 	// NOTE: Outer loop is the character due to cache locality.
-	for (int chr = NUM_ELEMENTS(levels[0].spnCount; chr >= 0); chr--) {
+	for (int chr = NUM_ELEMENTS(levels[0].spnCount); chr >= 0; chr--) {
 		for (int level = NUM_ELEMENTS(levels); level >= 0; level--) {
 			levels[level].spnCount[chr]->setValue(clear_count.all[chr][level]);
 		}