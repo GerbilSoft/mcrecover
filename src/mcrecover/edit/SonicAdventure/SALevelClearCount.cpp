GerbilSoft/mcrecover

commit eec066a32ee5d82aae9af2048c1e55d96a145df0

[SonicAdventure] SALevelClearCount: Make sure the widgets stay at the top of the scroll area.
Added a QSpacerItem at the bottom to ensure that if the window is
expanded past the total size of the QScrollArea, the widgets stay
at the top instead of adding space between each row.

New #define TOTAL_CHARACTERS. Use this macro throughout the file
wherever the total number of characters is needed.

SALevelClearCount.ui: Mark scrlLevelsContent as H=Expanding, V=Minimum.
This didn't help with spacing (the QSpacerItem was still needed), but
it should still be set.

src/mcrecover/edit/SonicAdventure/SALevelClearCount.cpp
@@ -64,11 +64,13 @@ class SALevelClearCountPrivate
 		// FIXME: sizeof(sa_level_clear_counts::clear[0]) works in gcc,
 		// but fails in MSVC 2010 with C2597.
 		#define TOTAL_LEVELS 43
+		#define TOTAL_CHARACTERS 8
 		// Level widgets.
 		struct {
 			QLabel *lblLevel;
-			QSpinBox *spnCount[8];
+			QSpinBox *spnCount[TOTAL_CHARACTERS];
 		} levels[TOTAL_LEVELS];
+		QSpacerItem *vspcSpacer;
 
 		// Signal mapper for QSpinBox modifications.
 		QSignalMapper *mapperSpinBox;
@@ -101,7 +103,7 @@ class SALevelClearCountPrivate
 		 * - Index = character ID (including unused)
 		 * - Value = bitfield
 		 */
-		static const uint64_t levelMap[8];
+		static const uint64_t levelMap[TOTAL_CHARACTERS];
 };
 
 /**
@@ -111,7 +113,7 @@ class SALevelClearCountPrivate
  * - Index = character ID (including unused)
  * - Value = bitfield
  */
-const uint64_t SALevelClearCountPrivate::levelMap[8] = {
+const uint64_t SALevelClearCountPrivate::levelMap[TOTAL_CHARACTERS] = {
 	// These values are based on my 100% save file.
 	// NOTE: Chao Gardens are all listed as 0.
 	// FIXME: Some values may be incorrect...
@@ -127,6 +129,7 @@ const uint64_t SALevelClearCountPrivate::levelMap[8] = {
 
 SALevelClearCountPrivate::SALevelClearCountPrivate(SALevelClearCount *q)
 	: q_ptr(q)
+	, vspcSpacer(nullptr)
 	, mapperSpinBox(new QSignalMapper(q))
 {
 	// Clear the data.
@@ -148,6 +151,7 @@ SALevelClearCountPrivate::~SALevelClearCountPrivate()
 			delete levels[i].spnCount[j];
 		}
 	}
+	delete vspcSpacer;
 }
 
 /**
@@ -175,7 +179,7 @@ void SALevelClearCountPrivate::initLevels(void)
 	pal.setColor(QPalette::Base, bgColor_unused);
 
 	// Level mappings.
-	uint64_t levelMap[8];
+	uint64_t levelMap[TOTAL_CHARACTERS];
 	memcpy(levelMap, this->levelMap, sizeof(levelMap));
 
 	// Create widgets for all levels.
@@ -210,7 +214,7 @@ void SALevelClearCountPrivate::initLevels(void)
 			levels[level].spnCount[chr]->setRange(0, 255);
 			levels[level].spnCount[chr]->setSingleStep(1);
 			levels[level].spnCount[chr]->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-			ui.gridLevels->addWidget(levels[level].spnCount[chr], level, chr+1, Qt::AlignTop);
+			ui.gridLevels->addWidget(levels[level].spnCount[chr], level, chr+1, Qt::AlignCenter);
 
 			if (!(levelMap[chr] & 1)) {
 				// Level is not used by this character.
@@ -228,6 +232,11 @@ void SALevelClearCountPrivate::initLevels(void)
 						((level << 8) | chr));
 		}
 	}
+
+	// Add a spacer to make sure everything is pushed to the top
+	// if the window is expanded.
+	vspcSpacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+	ui.gridLevels->addItem(vspcSpacer, TOTAL_LEVELS, 0, 1, TOTAL_CHARACTERS+1);
 }
 
 /**
@@ -342,7 +351,7 @@ void SALevelClearCount::spnCount_mapped_slot(int spnId)
 	// Save the data for the spinbox.
 	const int chr = (spnId & 0xFF);
 	const int level = (spnId >> 8);
-	if (chr < 0 || chr >= 8 || level < 0 || level >= TOTAL_LEVELS)
+	if (chr < 0 || chr >= TOTAL_CHARACTERS || level < 0 || level >= TOTAL_LEVELS)
 		return;
 
 	Q_D(SALevelClearCount);

src/mcrecover/edit/SonicAdventure/SALevelClearCount.ui
@@ -162,11 +162,13 @@
         <height>66</height>
        </rect>
       </property>
-      <layout class="QGridLayout" name="gridLevels">
-       <property name="margin">
-        <number>0</number>
-       </property>
-      </layout>
+      <property name="sizePolicy">
+       <sizepolicy hsizetype="Expanding" vsizetype="Minimum">
+        <horstretch>0</horstretch>
+        <verstretch>0</verstretch>
+       </sizepolicy>
+      </property>
+      <layout class="QGridLayout" name="gridLevels"/>
      </widget>
     </widget>
    </item>

commit f9156da80c9b52d2ce986bdf1074374558d6d14c

[SonicAdventure] SALevelClearCount: Don't delete vspcSpacer manually.
QGridLayout takes ownership, so if we delete it manually, QGridLayout
ends up crashing because it tries to delete it itself.

This is a regression from commit eec066a32ee5d82aae9af2048c1e55d96a145df0.
([SonicAdventure] SALevelClearCount: Make sure the widgets stay at the top of the scroll area.)

src/mcrecover/edit/SonicAdventure/SALevelClearCount.cpp
@@ -71,7 +71,6 @@ class SALevelClearCountPrivate
 			QLabel *lblLevel;
 			QSpinBox *spnCount[TOTAL_CHARACTERS];
 		} levels[TOTAL_LEVELS];
-		QSpacerItem *vspcSpacer;
 
 		// Signal mapper for QSpinBox modifications.
 		QSignalMapper *mapperSpinBox;
@@ -136,7 +135,6 @@ const uint64_t SALevelClearCountPrivate::levelMap[TOTAL_CHARACTERS] = {
 
 SALevelClearCountPrivate::SALevelClearCountPrivate(SALevelClearCount *q)
 	: q_ptr(q)
-	, vspcSpacer(nullptr)
 	, mapperSpinBox(new QSignalMapper(q))
 {
 	// Clear the data.
@@ -158,7 +156,6 @@ SALevelClearCountPrivate::~SALevelClearCountPrivate()
 			delete levels[i].spnCount[j];
 		}
 	}
-	delete vspcSpacer;
 }
 
 /**
@@ -242,7 +239,8 @@ void SALevelClearCountPrivate::initLevels(void)
 
 	// Add a spacer to make sure everything is pushed to the top
 	// if the window is expanded.
-	vspcSpacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
+	// NOTE: QGridLayout takes ownership, so we shouldn't delete it ourselves.
+	QSpacerItem *vspcSpacer = new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding);
 	ui.gridLevels->addItem(vspcSpacer, TOTAL_LEVELS, 0, 1, TOTAL_CHARACTERS+1);
 }
 