GerbilSoft/mcrecover

commit c9c47e2ceb41c7145b33eb4492573a33574dc96b

[edit] ByteFlags: Added support for translations.
SADXDataMissions.c: Added QT_TRANSLATE_NOOP() using "SADXDataMissions"
as the context.

src/mcrecover/edit/SonicAdventure/SADXMissionFlags.cpp
@@ -54,7 +54,11 @@ SADXMissionFlagsPrivate::SADXMissionFlagsPrivate()
 /** SADXMissionFlags **/
 
 SADXMissionFlags::SADXMissionFlags(QObject *parent)
-	: ByteFlags(SADX_MISSION_COUNT, &sadx_mission_flags_desc[0], NUM_ELEMENTS(sadx_mission_flags_desc)-1, parent)
+	: ByteFlags(512,					// Total number of byte flags.
+		"SADXDataMissions",				// Translation context.
+		&sadx_mission_flags_desc[0],			// Bit flags with names.
+		NUM_ELEMENTS(sadx_mission_flags_desc)-1,	// Number of named flags.
+		parent)
 	, d_ptr(new SADXMissionFlagsPrivate())
 { }
 

src/mcrecover/edit/SonicAdventure/SADXDataMissions.c
@@ -22,79 +22,83 @@
 
 #include "SAData.h"
 
+#ifndef QT_TRANSLATE_NOOP
+#define QT_TRANSLATE_NOOP(ctx, str) str
+#endif
+
 // Mission flags.
 const bit_flag_t sadx_mission_flags_desc[SADX_MISSION_FLAGS_COUNT+1] = {
 	// Missions 1-10
-	{0, "Bring the man who is standing in front of the hamburger shop!"},
-	{1, "Get the balloon in the skies of the Mystic Ruins!"},
-	{2, "Collect 100 rings, and go to Sonic's billboard by the pool!"},
-	{3, "Weeds are growing all over my place! I must get rid of them!"},
-	{4, "I lost my balloon! It's way up there now!"},
-	{5, "He is going to drown! Help the man in the water!"},
-	{6, "Lonely Metal Sonic needs a friend. Look carefully."},
-	{7, "The medallion fell under there! No illegal parking please!"},
-	{8, "Get the balloon floating behind the waterfall at the emerald sea."},
-	{9, "What is that sparkling in the water?"},
+	{0, QT_TRANSLATE_NOOP("SADXDataMissions", "Bring the man who is standing in front of the hamburger shop!")},
+	{1, QT_TRANSLATE_NOOP("SADXDataMissions", "Get the balloon in the skies of the Mystic Ruins!")},
+	{2, QT_TRANSLATE_NOOP("SADXDataMissions", "Collect 100 rings, and go to Sonic's billboard by the pool!")},
+	{3, QT_TRANSLATE_NOOP("SADXDataMissions", "Weeds are growing all over my place! I must get rid of them!")},
+	{4, QT_TRANSLATE_NOOP("SADXDataMissions", "I lost my balloon! It's way up there now!")},
+	{5, QT_TRANSLATE_NOOP("SADXDataMissions", "He is going to drown! Help the man in the water!")},
+	{6, QT_TRANSLATE_NOOP("SADXDataMissions", "Lonely Metal Sonic needs a friend. Look carefully.")},
+	{7, QT_TRANSLATE_NOOP("SADXDataMissions", "The medallion fell under there! No illegal parking please!")},
+	{8, QT_TRANSLATE_NOOP("SADXDataMissions", "Get the balloon floating behind the waterfall at the emerald sea.")},
+	{9, QT_TRANSLATE_NOOP("SADXDataMissions", "What is that sparkling in the water?")},
 
 	// Missions 11-20
-	{10, "Destroy the windmill and proceed. Find the balloon in orbit!"},
-	{11, "Who is a Chao good friends with? And what is hidden underneath?"},
-	{12, "I can't take a shower like this! Do something!"},
-	{13, "I am the keeper of this hotel! Catch me if you can!"},
-	{14, "My medallions got swept away by the tornado! Somebody help me get them back!"},
-	{15, "Get the flags from the floating islands!"},
-	{16, "Aim and shoot all the medallions with a Sonic Ball."},
-	{17, "During the night, at the amusement park, place your jumps on the top of one of the tables."},
-	{18, "What is that behind the mirror?"},
-	{19, "Get all the medallions within the time limit! It's real slippery, so be careful!"},
+	{10, QT_TRANSLATE_NOOP("SADXDataMissions", "Destroy the windmill and proceed. Find the balloon in orbit!")},
+	{11, QT_TRANSLATE_NOOP("SADXDataMissions", "Who is a Chao good friends with? And what is hidden underneath?")},
+	{12, QT_TRANSLATE_NOOP("SADXDataMissions", "I can't take a shower like this! Do something!")},
+	{13, QT_TRANSLATE_NOOP("SADXDataMissions", "I am the keeper of this hotel! Catch me if you can!")},
+	{14, QT_TRANSLATE_NOOP("SADXDataMissions", "My medallions got swept away by the tornado! Somebody help me get them back!")},
+	{15, QT_TRANSLATE_NOOP("SADXDataMissions", "Get the flags from the floating islands!")},
+	{16, QT_TRANSLATE_NOOP("SADXDataMissions", "Aim and shoot all the medallions with a Sonic Ball.")},
+	{17, QT_TRANSLATE_NOOP("SADXDataMissions", "During the night, at the amusement park, place your jumps on the top of one of the tables.")},
+	{18, QT_TRANSLATE_NOOP("SADXDataMissions", "What is that behind the mirror?")},
+	{19, QT_TRANSLATE_NOOP("SADXDataMissions", "Get all the medallions within the time limit! It's real slippery, so be careful!")},
 
 	// Missions 21-30
-	{20, "Protect the Sonic doll from the Spinners surrounding it!"},
-	{21, "Find the flag hidden in the secret passage under the emerald ocean!"},
-	{22, "Go around the wooden horse and collect 10 balloons."},
-	{23, "'I hate this dark and filthy place!' Can you find it?"},
-	{24, "What is hidden under the lion's right hand?"},
-	{25, "What is that on top of the ship's mast that the pirates are protecting?"},
-	{26, "Collect 100 rings and head to the heliport!"},
-	{27, "During the morning traffic, use the fountain to get the balloon."},
-	{28, "I am the keeper of this canal! Catch me if you can!"},
-	{29, "A fugitive have escaped from the jail of burning hell! Find the fugitive!"},
+	{20, QT_TRANSLATE_NOOP("SADXDataMissions", "Protect the Sonic doll from the Spinners surrounding it!")},
+	{21, QT_TRANSLATE_NOOP("SADXDataMissions", "Find the flag hidden in the secret passage under the emerald ocean!")},
+	{22, QT_TRANSLATE_NOOP("SADXDataMissions", "Go around the wooden horse and collect 10 balloons.")},
+	{23, QT_TRANSLATE_NOOP("SADXDataMissions", "'I hate this dark and filthy place!' Can you find it?")},
+	{24, QT_TRANSLATE_NOOP("SADXDataMissions", "What is hidden under the lion's right hand?")},
+	{25, QT_TRANSLATE_NOOP("SADXDataMissions", "What is that on top of the ship's mast that the pirates are protecting?")},
+	{26, QT_TRANSLATE_NOOP("SADXDataMissions", "Collect 100 rings and head to the heliport!")},
+	{27, QT_TRANSLATE_NOOP("SADXDataMissions", "During the morning traffic, use the fountain to get the balloon.")},
+	{28, QT_TRANSLATE_NOOP("SADXDataMissions", "I am the keeper of this canal! Catch me if you can!")},
+	{29, QT_TRANSLATE_NOOP("SADXDataMissions", "A fugitive have escaped from the jail of burning hell! Find the fugitive!")},
 
 	// Missions 31-40
-	{30, "Get the balloon as you float in the air along with the trash!"},
-	{31, "Can you get the balloon that is hidden under the bridge?"},
-	{32, "Shoot yourself out of the cannon and get the balloon!"},
-	{33, "Can you find the balloon that is hidden on the ship's bridge?"},
-	{34, "I am the keeper of this icy lake! Catch me if you can!"},
-	{35, "Fighter aircraft are flying everywhere. Somebody get me out of here!"},
-	{36, "Fly over the jungle, and get all the balloons!"},
-	{37, "A message from an ancient people: In the direction where the burning arrow is pointing, you will see..."},
-	{38, "Treasure hunt at the beach! Find all the medallions under a time limit!"},
-	{39, "What is hidden in the area that the giant snake is staring at?"},
+	{30, QT_TRANSLATE_NOOP("SADXDataMissions", "Get the balloon as you float in the air along with the trash!")},
+	{31, QT_TRANSLATE_NOOP("SADXDataMissions", "Can you get the balloon that is hidden under the bridge?")},
+	{32, QT_TRANSLATE_NOOP("SADXDataMissions", "Shoot yourself out of the cannon and get the balloon!")},
+	{33, QT_TRANSLATE_NOOP("SADXDataMissions", "Can you find the balloon that is hidden on the ship's bridge?")},
+	{34, QT_TRANSLATE_NOOP("SADXDataMissions", "I am the keeper of this icy lake! Catch me if you can!")},
+	{35, QT_TRANSLATE_NOOP("SADXDataMissions", "Fighter aircraft are flying everywhere. Somebody get me out of here!")},
+	{36, QT_TRANSLATE_NOOP("SADXDataMissions", "Fly over the jungle, and get all the balloons!")},
+	{37, QT_TRANSLATE_NOOP("SADXDataMissions", "A message from an ancient people: In the direction where the burning arrow is pointing, you will see...")},
+	{38, QT_TRANSLATE_NOOP("SADXDataMissions", "Treasure hunt at the beach! Find all the medallions under a time limit!")},
+	{39, QT_TRANSLATE_NOOP("SADXDataMissions", "What is hidden in the area that the giant snake is staring at?")},
 
 	// Missions 41-50
-	{40, "Look real carefully just as you fall from the waterfall!"},
-	{41, "I can't get into the bathroom. How could I've let something like this happen to me?"},
-	{42, "Fortress of steel. High Jump on 3 narrow paths. Be careful not to fall."},
-	{43, "I am the keeper of this ship! Catch me if you can!"},
-	{44, "Go to a place where the rings are laid in the shape of Sonic's face!"},
-	{45, "A secret base full of mechanical traps. Pay attention, and you might see..."},
-	{46, "Get 10 balloons on the field under the time limit!"},
-	{47, "Can you get the medallion that the giant Sonic is staring at?"},
-	{48, "Scorch through the track, and get all the flags!"},
-	{49, "Select a road that splits into 5 paths before time runs out!"},
+	{40, QT_TRANSLATE_NOOP("SADXDataMissions", "Look real carefully just as you fall from the waterfall!")},
+	{41, QT_TRANSLATE_NOOP("SADXDataMissions", "I can't get into the bathroom. How could I've let something like this happen to me?")},
+	{42, QT_TRANSLATE_NOOP("SADXDataMissions", "Fortress of steel. High Jump on 3 narrow paths. Be careful not to fall.")},
+	{43, QT_TRANSLATE_NOOP("SADXDataMissions", "I am the keeper of this ship! Catch me if you can!")},
+	{44, QT_TRANSLATE_NOOP("SADXDataMissions", "Go to a place where the rings are laid in the shape of Sonic's face!")},
+	{45, QT_TRANSLATE_NOOP("SADXDataMissions", "A secret base full of mechanical traps. Pay attention, and you might see...")},
+	{46, QT_TRANSLATE_NOOP("SADXDataMissions", "Get 10 balloons on the field under the time limit!")},
+	{47, QT_TRANSLATE_NOOP("SADXDataMissions", "Can you get the medallion that the giant Sonic is staring at?")},
+	{48, QT_TRANSLATE_NOOP("SADXDataMissions", "Scorch through the track, and get all the flags!")},
+	{49, QT_TRANSLATE_NOOP("SADXDataMissions", "Select a road that splits into 5 paths before time runs out!")},
 
 	// Missions 51-60
-	{50, "Gunman of the Windy Valley! Destroy all of the Spinners under a time limit!"},
-	{51, "Get 3 flags in the jungle under the time limit!"},
-	{52, "Pass the line of rings with 3 Super High Jumps on the ski slope!"},
-	{53, "Slide downhill in a blizzard and get all of the flags!"},
-	{54, "Run down the building to get all the balloons!"},
-	{55, "Relentless eruptions occur in the flaming canyon. What could be hidden in the area she's staring at?"},
-	{56, "Peak of the volcanic mountain! Watch out for the lava!"},
-	{57, "The big rock will start rolling after you! Try to get all the flags"},
-	{58, "Watch out for the barrels, and find the hidden flag inside the container!"},
-	{59, "Something is hidden inside the dinosaur's mouth. Can you find it?"},
+	{50, QT_TRANSLATE_NOOP("SADXDataMissions", "Gunman of the Windy Valley! Destroy all of the Spinners under a time limit!")},
+	{51, QT_TRANSLATE_NOOP("SADXDataMissions", "Get 3 flags in the jungle under the time limit!")},
+	{52, QT_TRANSLATE_NOOP("SADXDataMissions", "Pass the line of rings with 3 Super High Jumps on the ski slope!")},
+	{53, QT_TRANSLATE_NOOP("SADXDataMissions", "Slide downhill in a blizzard and get all of the flags!")},
+	{54, QT_TRANSLATE_NOOP("SADXDataMissions", "Run down the building to get all the balloons!")},
+	{55, QT_TRANSLATE_NOOP("SADXDataMissions", "Relentless eruptions occur in the flaming canyon. What could be hidden in the area she's staring at?")},
+	{56, QT_TRANSLATE_NOOP("SADXDataMissions", "Peak of the volcanic mountain! Watch out for the lava!")},
+	{57, QT_TRANSLATE_NOOP("SADXDataMissions", "The big rock will start rolling after you! Try to get all the flags")},
+	{58, QT_TRANSLATE_NOOP("SADXDataMissions", "Watch out for the barrels, and find the hidden flag inside the container!")},
+	{59, QT_TRANSLATE_NOOP("SADXDataMissions", "Something is hidden inside the dinosaur's mouth. Can you find it?")},
 
 	// End of list.
 	{-1, nullptr}

src/mcrecover/edit/models/ByteFlags.cpp
@@ -24,6 +24,7 @@
 #include "ByteFlags.hpp"
 
 // Qt includes.
+#include <QtCore/QCoreApplication>
 #include <QtCore/QHash>
 #include <QtCore/QString>
 #include <QtCore/QVector>
@@ -42,10 +43,12 @@ class ByteFlagsPrivate
 		/**
 		 * Initialize ByteFlagsPrivate.
 		 * @param total_flags Total number of flags the user can edit.
+		 * @param tr_ctx Translation context for bit flag descriptions.
 		 * @param byte_flags Byte flag descriptions.
 		 * @param count Number of bit_flags entries. (must be >= total_flags)
 		 */
-		ByteFlagsPrivate(int total_flags, const bit_flag_t *byte_flags, int count);
+		ByteFlagsPrivate(int total_flags, const char *tr_ctx,
+				 const bit_flag_t *byte_flags, int count);
 
 	private:
 		Q_DISABLE_COPY(ByteFlagsPrivate)
@@ -57,13 +60,25 @@ class ByteFlagsPrivate
 		// of said class are deleted?
 		// TODO: An array might be more efficient, even if
 		// it wastes some memory...
-		QHash<int, QString> objs_desc;
+		QHash<int, const char*> objs_desc;
 
 		// Objects, each with 8 flags.
 		QVector<uint8_t> objs;
+		
+		// Translation context for bit flags.
+		const char *tr_ctx;
 };
 
-ByteFlagsPrivate::ByteFlagsPrivate(int total_flags, const bit_flag_t *byte_flags, int count)
+/**
+ * Initialize ByteFlagsPrivate.
+ * @param total_flags Total number of flags the user can edit.
+ * @param tr_ctx Translation context for bit flag descriptions.
+ * @param byte_flags Byte flag descriptions.
+ * @param count Number of bit_flags entries. (must be >= total_flags)
+ */
+ByteFlagsPrivate::ByteFlagsPrivate(int total_flags, const char *tr_ctx,
+				   const bit_flag_t *byte_flags, int count)
+	: tr_ctx(tr_ctx)
 {
 	// This is initialized by a derived private class.
 	assert(total_flags > 0);
@@ -85,7 +100,7 @@ ByteFlagsPrivate::ByteFlagsPrivate(int total_flags, const bit_flag_t *byte_flags
 			break;
 		}
 
-		objs_desc.insert(byte_flags->event, QLatin1String(byte_flags->description));
+		objs_desc.insert(byte_flags->event, byte_flags->description);
 	}
 }
 
@@ -102,9 +117,28 @@ ByteFlagsPrivate::ByteFlagsPrivate(int total_flags, const bit_flag_t *byte_flags
  * @param count Number of bit_flags entries. (must be >= total_flags)
  * @param parent Parent object.
  */
-ByteFlags::ByteFlags(int total_flags, const bit_flag_t *byte_flags, int count, QObject *parent)
+ByteFlags::ByteFlags(int total_flags, const bit_flag_t *byte_flags,
+		     int count, QObject *parent)
 	: QObject(parent)
-	, d_ptr(new ByteFlagsPrivate(total_flags, byte_flags, count))
+	, d_ptr(new ByteFlagsPrivate(total_flags, nullptr, byte_flags, count))
+{ }
+
+/**
+ * Initialize ByteFlags.
+ *
+ * This should be called by subclass constructors with
+ * the appropriate values.
+ *
+ * @param total_flags Total number of flags the user can edit.
+ * @param tr_ctx Translation context for byte flag descriptions.
+ * @param byte_flags Byte flag descriptions.
+ * @param count Number of bit_flags entries. (must be >= total_flags)
+ * @param parent Parent object.
+ */
+ByteFlags::ByteFlags(int total_flags, const char *tr_ctx,
+		     const bit_flag_t *byte_flags, int count, QObject *parent)
+	: QObject(parent)
+	, d_ptr(new ByteFlagsPrivate(total_flags, tr_ctx, byte_flags, count))
 { }
 
 ByteFlags::~ByteFlags()
@@ -134,7 +168,22 @@ QString ByteFlags::description(int id) const
 		return tr("Invalid object ID");
 
 	Q_D(const ByteFlags);
-	return d->objs_desc.value(id, tr("Unknown"));
+	const char *desc = d->objs_desc.value(id);
+	if (!desc) {
+		// No flag description is available.
+		return tr("Unknown");
+	}
+
+	if (d->tr_ctx) {
+		// Translation context is available.
+		return QCoreApplication::translate(d->tr_ctx, desc);
+	}
+
+	// Translation context is not available.
+	// NOTE: This may end up being slower and/or
+	// using more memory due to lack of implicit
+	// QString sharing.
+	return QLatin1String(desc);
 }
 
 /**

src/mcrecover/edit/models/ByteFlags.hpp
@@ -53,7 +53,23 @@ class ByteFlags : public QObject
 		 * @param count Number of bit_flags entries. (must be >= total_flags)
 		 * @param parent Parent object.
 		 */
-		ByteFlags(int total_flags, const bit_flag_t *byte_flags, int count, QObject *parent = 0);
+		ByteFlags(int total_flags, const bit_flag_t *byte_flags,
+			  int count, QObject *parent = 0);
+
+		/**
+		 * Initialize ByteFlags.
+		 *
+		 * This should be called by subclass constructors with
+		 * the appropriate values.
+		 *
+		 * @param total_flags Total number of flags the user can edit.
+		 * @param tr_ctx Translation context for byte flag descriptions.
+		 * @param byte_flags Byte flag descriptions.
+		 * @param count Number of bit_flags entries. (must be >= total_flags)
+		 * @param parent Parent object.
+		 */
+		ByteFlags(int total_flags, const char *tr_ctx,
+			  const bit_flag_t *byte_flags, int count, QObject *parent = 0);
 	public:
 		virtual ~ByteFlags();
 

commit 084ad23b49e83c1d8e5c9a8812678cba8f6fabe0

[SonicAdventure] SADXMissionFlags: Set the correct number of flags.
Should be SADX_MISSION_COUNT, not 512. With 512, ByteFlagsView splits
it into 8 tabs with 64 missions each, using the same names as
SAEventFlags.

NOTE: ByteFlagsView still has hard-coded pages; it hasn't been updated
with the BitFlags changes from commit e76c30c6b378bd8e6d00e802df7be8d1b2407ada.
([SonicAdventure] BitFlags: New virtual functions pageSize() and pageName().)

This fixes a regression from commit c9c47e2ceb41c7145b33eb4492573a33574dc96b.
([edit] ByteFlags: Added support for translations.)

src/mcrecover/edit/SonicAdventure/SADXMissionFlags.cpp
@@ -54,7 +54,7 @@ SADXMissionFlagsPrivate::SADXMissionFlagsPrivate()
 /** SADXMissionFlags **/
 
 SADXMissionFlags::SADXMissionFlags(QObject *parent)
-	: ByteFlags(512,					// Total number of byte flags.
+	: ByteFlags(SADX_MISSION_COUNT,				// Total number of byte flags.
 		"SADXDataMissions",				// Translation context.
 		&sadx_mission_flags_desc[0],			// Bit flags with names.
 		NUM_ELEMENTS(sadx_mission_flags_desc)-1,	// Number of named flags.