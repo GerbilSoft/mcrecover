6b6847ecf42f3abf2dc99bd3d17f0caf91c74880
David Korth <gerbilsoft@gerbilsoft.com>
2016-12-16 01:11:17 -0500

    appveyor.cmd: Configure the CMake prefix path for Qt.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF "%PLATFORM%" == "x64" (
-	cmake . -G "Visual Studio 14 2015 Win64" -DUSE_QT4=ON -DUSE_QT5=OFF
+	cmake . -G "Visual Studio 14 2015 Win64" -DUSE_QT4=ON -DUSE_QT5=OFF -DCMAKE_PREFIX_PATH="C:/Qt/5.7/msvc2015_64"
 ) ELSE (
-	cmake . -G "Visual Studio 14 2015" -DUSE_QT4=ON -DUSE_QT5=OFF
+	cmake . -G "Visual Studio 14 2015" -DUSE_QT4=ON -DUSE_QT5=OFF -DCMAKE_PREFIX_PATH="C:/Qt/5.7/msvc2015"
 )
