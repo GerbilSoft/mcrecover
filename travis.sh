5235d13842fa919f6477f8fcb62510dd3099666a
David Korth <gerbilsoft@gerbilsoft.com>
2016-12-16 01:02:22 -0500

    .travis.yml, travis.sh: Actually update for mcrecover.
    These were copied from rom-properties, but the changes didn't get
    committed, so the build failed.

--- travis.sh
+++ travis.sh
@@ -4,11 +4,8 @@ mkdir "${TRAVIS_BUILD_DIR}/build"
 cd "${TRAVIS_BUILD_DIR}/build"
 cmake .. \
 	-DCMAKE_INSTALL_PREFIX=/usr \
-	-DBUILD_TESTING=ON\
+	-DUSE_QT4=ON \
+	-DUSE_QT5=OFF \
 	|| exit 1
-# Build UTF-8 and UTF-16 libraries for testing purposes.
-make romdata8 romdata16 cachemgr8 cachemgr16 || RET=1
-# Build the actual plugin(s).
 make || RET=1
-ctest -V || RET=1
 exit "${RET}"
