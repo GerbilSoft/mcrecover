f16843c4483689a461a8a5745bd4ed7bd60dfb72
David Korth <gerbilsoft@gerbilsoft.com>
2016-12-16 01:06:10 -0500

    travis.sh: Set CMAKE_BUILD_TYPE=Release.

--- travis.sh
+++ travis.sh
@@ -4,6 +4,7 @@ mkdir "${TRAVIS_BUILD_DIR}/build"
 cd "${TRAVIS_BUILD_DIR}/build"
 cmake .. \
 	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DCMAKE_BUILD_TYPE=Release \
 	-DUSE_QT4=ON \
 	-DUSE_QT5=OFF \
 	|| exit 1
