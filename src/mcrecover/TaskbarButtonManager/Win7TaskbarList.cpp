GerbilSoft/mcrecover

commit 2b0764937b096894662eac56f92c27a06ad22a94

Use static_cast<> and qobject_cast<> instead of reinterpret_cast<> where possible.

src/mcrecover/TaskbarButtonManager/Win7TaskbarList.cpp
@@ -90,7 +90,7 @@ void Win7TaskbarListPrivate::update(void)
 		return;
 
 	// TODO: winId(), or effectiveWinId()?
-	HWND hWnd = reinterpret_cast<HWND>(window->winId());
+	HWND hWnd = static_cast<HWND>(window->winId());
 	if (!hWnd)
 		return;
 

src/libgctools/Checksum.cpp
@@ -228,17 +228,17 @@ uint32_t Checksum::Exec(ChkAlgorithm algorithm, const void *buf, uint32_t siz, C
 		case CHKALG_CRC16:
 			if (param == 0)
 				param = CRC16_POLY_CCITT;
-			return Crc16(reinterpret_cast<const uint8_t*>(buf),
+			return Crc16(static_cast<const uint8_t*>(buf),
 				     siz, (uint16_t)(param & 0xFFFF));
 
 		case CHKALG_ADDINVDUAL16:
-			return AddInvDual16(reinterpret_cast<const uint16_t*>(buf), siz, endian);
+			return AddInvDual16(static_cast<const uint16_t*>(buf), siz, endian);
 
 		case CHKALG_ADDBYTES32:
-			return AddBytes32(reinterpret_cast<const uint8_t*>(buf), siz);
+			return AddBytes32(static_cast<const uint8_t*>(buf), siz);
 
 		case CHKALG_SONICCHAOGARDEN:
-			return SonicChaoGarden(reinterpret_cast<const uint8_t*>(buf), siz);
+			return SonicChaoGarden(static_cast<const uint8_t*>(buf), siz);
 
 		case CHKALG_DREAMCASTVMU:
 			// If param is 0, assume a default CRC address of 0x46.
@@ -246,7 +246,7 @@ uint32_t Checksum::Exec(ChkAlgorithm algorithm, const void *buf, uint32_t siz, C
 			//  but the CRC field is unused for game files.)
 			if (param == 0)
 				param = 0x46;
-			return DreamcastVMU(reinterpret_cast<const uint8_t*>(buf), siz, param);
+			return DreamcastVMU(static_cast<const uint8_t*>(buf), siz, param);
 
 		case CHKALG_CRC32:
 			// TODO: Implement CRC32 once I encounter a file that uses it.

src/libgctools/GcImage.cpp
@@ -130,8 +130,8 @@ GcImage *GcImage::toRGB5A3(void) const
 			GcImagePrivate *const d_new = gcImage->d;
 			d_new->init(d->width, d->height, PXFMT_ARGB32);
 
-			const uint8_t *ci8 = reinterpret_cast<const uint8_t*>(d->imageData);
-			uint32_t *rgb5A3 = reinterpret_cast<uint32_t*>(d_new->imageData);
+			const uint8_t *ci8 = static_cast<const uint8_t*>(d->imageData);
+			uint32_t *rgb5A3 = static_cast<uint32_t*>(d_new->imageData);
 			size_t len = d->imageData_len;
 			for (; len >= 4; len -= 4, ci8 += 4, rgb5A3 += 4) {
 				*(rgb5A3 + 0) = d->palette[*(ci8 + 0)];

src/libgctools/GcImageWriter_GIF.cpp
@@ -63,7 +63,7 @@ int GcImageWriterPrivate::gif_output_func(GifFileType *gif, const GifByteType *b
 		return 0;
 
 	// Assuming the UserData is a vector<uint8_t>*.
-	vector<uint8_t> *gifBuffer = reinterpret_cast<vector<uint8_t>*>(userData);
+	vector<uint8_t> *gifBuffer = static_cast<vector<uint8_t>*>(userData);
 	size_t pos = gifBuffer->size();
 	gifBuffer->resize(pos + len);
 	memcpy(&gifBuffer->data()[pos], buf, len);

src/libgctools/GcImageWriter_PNG.cpp
@@ -51,7 +51,7 @@ void GcImageWriterPrivate::png_io_write(png_structp png_ptr, png_bytep buf, png_
 		return;
 
 	// Assuming the io_ptr is a vector<uint8_t>*.
-	vector<uint8_t> *pngBuffer = reinterpret_cast<vector<uint8_t>*>(io_ptr);
+	vector<uint8_t> *pngBuffer = static_cast<vector<uint8_t>*>(io_ptr);
 	size_t pos = pngBuffer->size();
 	pngBuffer->resize(pos + len);
 	memcpy(&pngBuffer->data()[pos], buf, len);

src/mcrecover/PcreRegex.cpp
@@ -49,11 +49,11 @@
 
 // Regex type.
 #ifdef HAVE_PCRE16
-#define REGEX_CAST(x) (reinterpret_cast<pcre16*>(x))
+#define REGEX_CAST(x) (static_cast<pcre16*>(x))
 #define pcre_config(what, where) pcre16_config(what, where)
 #define pcre_free(ptr) pcre16_free(ptr)
 #else /* !HAVE_PCRE16 */
-#define REGEX_CAST(x) (reinterpret_cast<pcre*>(x))
+#define REGEX_CAST(x) (static_cast<pcre*>(x))
 #endif
 
 PcreRegex::PcreRegex()

src/mcrecover/edit/SonicAdventure/SALevelClearCount.cpp
@@ -276,7 +276,7 @@ void SALevelClearCountPrivate::updateDisplay(void)
 void SALevelClearCountPrivate::scrollAreaResized(QEvent *event)
 {
 	if (event->type() == QEvent::Resize) {
-		QResizeEvent *resizeEvent = reinterpret_cast<QResizeEvent*>(event);
+		QResizeEvent *resizeEvent = static_cast<QResizeEvent*>(event);
 		if (resizeEvent->oldSize().width() == resizeEvent->size().width()) {
 			// Vertical resize only.
 			return;

src/mcrecover/widgets/MessageWidgetStack.cpp
@@ -143,8 +143,8 @@ void MessageWidgetStack::messageWidget_dismissed_slot(QWidget *widget)
 {
 	// Remove and delete the widget.
 	Q_D(MessageWidgetStack);
-	if (d->messageWidgets.contains(reinterpret_cast<MessageWidget*>(widget))) {
-		d->messageWidgets.remove(reinterpret_cast<MessageWidget*>(widget));
+	if (d->messageWidgets.contains(qobject_cast<MessageWidget*>(widget))) {
+		d->messageWidgets.remove(qobject_cast<MessageWidget*>(widget));
 		delete widget;
 	}
 }
@@ -157,6 +157,6 @@ void MessageWidgetStack::messageWidget_destroyed_slot(QObject *obj)
 {
 	// Remove the widget.
 	Q_D(MessageWidgetStack);
-	d->messageWidgets.remove(reinterpret_cast<MessageWidget*>(obj));
+	d->messageWidgets.remove(qobject_cast<MessageWidget*>(obj));
 }
 

commit bc3fbf15b266d29da742cf6ab9e18d51fab1f17b

[mcrecover] Win7TaskbarList.cpp: Revert one static_cast<> to reinterpret_cast<>.
MSVC 2010 complains about this static_cast<>.

This fixes a regression from commit 2b0764937b096894662eac56f92c27a06ad22a94.
(Use static_cast<> and qobject_cast<> instead of reinterpret_cast<> where possible.)

src/mcrecover/TaskbarButtonManager/Win7TaskbarList.cpp
@@ -90,7 +90,7 @@ void Win7TaskbarListPrivate::update(void)
 		return;
 
 	// TODO: winId(), or effectiveWinId()?
-	HWND hWnd = static_cast<HWND>(window->winId());
+	HWND hWnd = reinterpret_cast<HWND>(window->winId());
 	if (!hWnd)
 		return;
 