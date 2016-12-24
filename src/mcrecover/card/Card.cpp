GerbilSoft/mcrecover

commit a4533a226de3d7f5b285ff0cb28567d1f09b5b33

[mcrecover] Moved the block/directory table index functions down to the Card base class.
The CardPrivate constructor has two new parameters for the total
directory and block table count. I don't expect any cards to have
a variable count (unlike e.g. FAT file systems).

VmuCard: Set generic values for DAT/BAT tables. I don't think VMUs have
more than one DAT or BAT. I'll need to double-chcek this later.

setActiveDatIdx() and setActiveBatIdx() are pure virtual.

TODO: Add notification signals for those functions?

src/mcrecover/card/Card.cpp
@@ -39,7 +39,9 @@
 
 /** CardPrivate **/
 
-CardPrivate::CardPrivate(Card *q, uint32_t blockSize, int minBlocks, int maxBlocks)
+CardPrivate::CardPrivate(Card *q, uint32_t blockSize,
+			 int minBlocks, int maxBlocks,
+			 int dat_count, int bat_count)
 	: q_ptr(q)
 	, errors(0)
 	, file(nullptr)
@@ -56,6 +58,20 @@ CardPrivate::CardPrivate(Card *q, uint32_t blockSize, int minBlocks, int maxBloc
 	assert(blockSize > 0);
 	assert(minBlocks > 0);
 	assert(maxBlocks >= minBlocks);
+	assert(dat_count > 0 && dat_count < 32);
+	assert(bat_count > 0 && bat_count < 32);
+
+	// Directory Table information.
+	dat_info.count = dat_count;
+	dat_info.active = -1;
+	dat_info.active_hdr = 0;
+	dat_info.valid = 0;
+
+	// Block Table information.
+	bat_info.count = dat_count;
+	bat_info.active = -1;
+	bat_info.active_hdr = 0;
+	bat_info.valid = 0;
 }
 
 CardPrivate::~CardPrivate()
@@ -331,6 +347,110 @@ GcnDateTime Card::formatTime(void) const
 		return GcnDateTime();
 	Q_D(const Card);
 	return d->formatTime;
+}
+
+/** File system **/
+
+/**
+ * Get the number of directory tables.
+ * @return Number of directory tables. (-1 on error)
+ */
+int Card::datCount(void) const
+{
+	if (!isOpen())
+		return -1;
+	Q_D(const Card);
+	return d->dat_info.count;
+}
+
+/**
+ * Get the active Directory Table index.
+ * @return Active Directory Table index. (-1 on error)
+ */
+int Card::activeDatIdx(void) const
+{
+	if (!isOpen())
+		return -1;
+	Q_D(const Card);
+	return d->dat_info.active;
+}
+
+/**
+ * Get the active Directory Table index according to the card header.
+ * @return Active Directory Table index, or -1 if all are invalid.
+ */
+int Card::activeDatHdrIdx(void) const
+{
+	if (!isOpen())
+		return -1;
+	Q_D(const Card);
+	return d->dat_info.active_hdr;
+}
+
+/**
+ * Is a Directory Table valid?
+ * @param idx Directory Table index.
+ * @return True if valid; false if not valid or idx is invalid.
+ */
+bool Card::isDatValid(int idx) const
+{
+	if (!isOpen())
+		return false;
+	Q_D(const Card);
+	if (idx < 0 || idx >= d->dat_info.count)
+		return false;
+	return d->isDatValid(idx);
+}
+
+/**
+ * Get the number of block tables.
+ * @return Number of block tables. (-1 on error)
+ */
+int Card::batCount(void) const
+{
+	if (!isOpen())
+		return -1;
+	Q_D(const Card);
+	return d->bat_info.count;
+}
+
+/**
+ * Get the active Block Table index.
+ * @return Active Block Table index. (-1 on error)
+ */
+int Card::activeBatIdx(void) const
+{
+	if (!isOpen())
+		return -1;
+	Q_D(const Card);
+	return d->bat_info.active;
+}
+
+/**
+ * Get the active Block Table index according to the card header.
+ * @return Active Block Table index, or -1 if both are invalid.
+ */
+int Card::activeBatHdrIdx(void) const
+{
+	if (!isOpen())
+		return -1;
+	Q_D(const Card);
+	return d->bat_info.active_hdr;
+}
+
+/**
+ * Is a Block Table valid?
+ * @param idx Block Table index.
+ * @return True if valid; false if not valid or idx is invalid.
+ */
+bool Card::isBatValid(int idx) const
+{
+	if (!isOpen())
+		return false;
+	Q_D(const Card);
+	if (idx < 0 || idx >= d->bat_info.count)
+		return false;
+	return d->isBatValid(idx);
 }
 
 /** Card I/O **/

src/mcrecover/card/Card.hpp
@@ -60,6 +60,15 @@ class Card : public QObject
 	Q_PROPERTY(QColor color READ color NOTIFY colorChanged)
 	Q_PROPERTY(GcnDateTime formatTime READ formatTime)
 
+	// File system.
+	// TODO: Notify signals for activeDatIdx / activeBatIdx?
+	Q_PROPERTY(int datCount READ datCount)
+	Q_PROPERTY(int activeDatIdx READ activeDatIdx WRITE setActiveDatIdx /*NOTIFY activeDatIdxChanged*/)
+	Q_PROPERTY(int activeDatHdrIdx READ activeDatHdrIdx)
+	Q_PROPERTY(int batCount READ batCount)
+	Q_PROPERTY(int activeBatIdx READ activeBatIdx WRITE setActiveBatIdx /*NOTIFY activeBatIdxChanged*/)
+	Q_PROPERTY(int activeBatHdrIdx READ activeBatHdrIdx)
+
 	// File management.
 	Q_PROPERTY(int fileCount READ fileCount)
 	Q_PROPERTY(bool empty READ isEmpty)
@@ -109,6 +118,7 @@ class Card : public QObject
 		 */
 		QString errorString(void) const;
 
+	public:
 		/** Card information **/
 
 		/**
@@ -201,6 +211,75 @@ class Card : public QObject
 		 */
 		GcnDateTime formatTime(void) const;
 
+	public:
+		/** File system **/
+		// TODO: Negative POSIX code on error, or just -1?
+
+		/**
+		 * Get the number of directory tables.
+		 * @return Number of directory tables. (-1 on error)
+		 */
+		int datCount(void) const;
+
+		/**
+		 * Get the active Directory Table index.
+		 * @return Active Directory Table index. (-1 on error)
+		 */
+		int activeDatIdx(void) const;
+
+		/**
+		 * Set the active Directory Table index.
+		 * NOTE: This function reloads the file list, without lost files.
+		 * @param idx Active Directory Table index.
+		 */
+		virtual void setActiveDatIdx(int idx) = 0;
+
+		/**
+		 * Get the active Directory Table index according to the card header.
+		 * @return Active Directory Table index, or -1 if both are invalid.
+		 */
+		int activeDatHdrIdx(void) const;
+
+		/**
+		 * Is a Directory Table valid?
+		 * @param idx Directory Table index.
+		 * @return True if valid; false if not valid or if idx is invalid.
+		 */
+		bool isDatValid(int idx) const;
+
+		/**
+		 * Get the number of block tables.
+		 * @return Number of block tables. (-1 on error)
+		 */
+		int batCount(void) const;
+
+		/**
+		 * Get the active Block Table index.
+		 * @return Active Block Table index. (-1 on error)
+		 */
+		int activeBatIdx(void) const;
+
+		/**
+		 * Set the active Block Table index.
+		 * NOTE: This function reloads the file list, without lost files.
+		 * @param idx Active Block Table index, or -1 if both are invalid.
+		 */
+		virtual void setActiveBatIdx(int idx) = 0;
+
+		/**
+		 * Get the active Block Table index according to the card header.
+		 * @return Active Block Table index, or -1 if both are invalid.
+		 */
+		int activeBatHdrIdx(void) const;
+
+		/**
+		 * Is a Block Table valid?
+		 * @param idx Block Table index.
+		 * @return True if valid; false if not valid or idx is invalid.
+		 */
+		bool isBatValid(int idx) const;
+
+	public:
 		/** Card I/O **/
 
 		/**

src/mcrecover/card/Card_p.hpp
@@ -35,7 +35,9 @@ class File;
 class CardPrivate
 {
 	public:
-		CardPrivate(Card *q, uint32_t blockSize, int minBlocks, int maxBlocks);
+		CardPrivate(Card *q, uint32_t blockSize,
+			    int minBlocks, int maxBlocks,
+			    int dat_count, int bat_count);
 		virtual ~CardPrivate();
 
 	protected:
@@ -66,6 +68,26 @@ class CardPrivate
 		int totalPhysBlocks;	// set by open()
 		int totalUserBlocks;	// must be set by subclass
 		int freeBlocks;		// must be set by subclass
+
+		// Table information.
+		// -1 indicates invalid.
+		struct tbl {
+			int count;	// Total number of this table.
+			int active;	// Which table is active?
+			int active_hdr;	// Which table is active, according to the header.
+			uint32_t valid;	// Bitfield indicating valid tables.
+		};
+
+		tbl dat_info;	// Directory Table information.
+		tbl bat_info;	// Block Table information.
+
+		// Useful inline functions to check if a DAT or BAT is valid.
+		inline bool isDatValid(int idx) const {
+			return !!(dat_info.valid & (1 << idx));
+		}
+		inline bool isBatValid(int idx) const {
+			return !!(bat_info.valid & (1 << idx));
+		}
 
 		// Files.
 		QVector<File*> lstFiles;

src/mcrecover/card/GcnCard.cpp
@@ -80,13 +80,6 @@ class GcnCardPrivate : public CardPrivate
 		uint32_t mc_dat_chk_expected[2];
 		uint32_t mc_bat_chk_actual[2];
 		uint32_t mc_bat_chk_expected[2];
-		bool mc_dat_valid[2];
-		bool mc_bat_valid[2];
-
-		// Active tables according to the card headers.
-		// 0, 1 == valid
-		//   -1 == both tables are invalid, default to 0
-		int8_t mc_dat_hdr_idx, mc_bat_hdr_idx;
 
 		// Active tables.
 		card_dat *mc_dat;
@@ -148,9 +141,12 @@ class GcnCardPrivate : public CardPrivate
 };
 
 GcnCardPrivate::GcnCardPrivate(GcnCard *q)
-	: CardPrivate(q, 8192, 64, 2048)
-	, mc_dat_hdr_idx(-1)
-	, mc_bat_hdr_idx(-1)
+	: CardPrivate(q,
+		8192,	// 8 KB blocks.
+		64,	// Minimum card size, in blocks.
+		2048,	// Maximum card size, in blocks.
+		2,	// Number of directory tables.
+		2)	// Number of block tables.
 	, mc_dat(nullptr)
 	, mc_bat(nullptr)
 {
@@ -162,8 +158,6 @@ GcnCardPrivate::GcnCardPrivate(GcnCard *q)
 	memset(mc_dat_chk_expected, 0, sizeof(mc_dat_chk_expected));
 	memset(mc_bat_chk_actual, 0, sizeof(mc_bat_chk_actual));
 	memset(mc_bat_chk_expected, 0, sizeof(mc_bat_chk_expected));
-	memset(mc_dat_valid, 0, sizeof(mc_dat_valid));
-	memset(mc_bat_valid, 0, sizeof(mc_bat_valid));
 }
 
 GcnCardPrivate::~GcnCardPrivate()
@@ -312,12 +306,15 @@ int GcnCardPrivate::format(const QString &filename)
 	mc_header.chksum2	= be16_to_cpu(mc_header.chksum2);
 	headerChecksumValue.actual = ((mc_header.chksum1 << 16) | mc_header.chksum2);
 	headerChecksumValue.expected = headerChecksumValue.actual;
+
+	dat_info.valid = 0;
+	bat_info.valid = 0;
 	for (int i = 0; i < 2; i++) {
 		// Directory Table.
 		mc_dat_int[i].dircntrl.updated = cpu_to_be16(mc_dat_int[i].dircntrl.updated);
 		mc_dat_int[i].dircntrl.chksum1 = cpu_to_be16(mc_dat_int[i].dircntrl.chksum1);
 		mc_dat_int[i].dircntrl.chksum2 = cpu_to_be16(mc_dat_int[i].dircntrl.chksum2);
-		mc_dat_valid[i] = true;
+		dat_info.valid |= (1 << i);
 
 		// Block Table.
 		mc_bat_int[i].updated		= cpu_to_be16(mc_bat_int[i].updated);
@@ -325,7 +322,7 @@ int GcnCardPrivate::format(const QString &filename)
 		mc_bat_int[i].lastalloc		= cpu_to_be16(mc_bat_int[i].lastalloc);
 		mc_bat_int[i].chksum1		= cpu_to_be16(mc_bat_int[i].chksum1);
 		mc_bat_int[i].chksum2		= cpu_to_be16(mc_bat_int[i].chksum2);
-		mc_bat_valid[i] = true;
+		bat_info.valid |= (1 << i);
 	}
 
 	// Reset the used block map.
@@ -454,6 +451,8 @@ int GcnCardPrivate::loadSysInfo(void)
 	// it will be zeroed out.
 	static const uint32_t DAT_addr[2] = {CARD_SYSDIR, CARD_SYSDIR_BACK};
 	static const uint32_t BAT_addr[2] = {CARD_SYSBAT, CARD_SYSBAT_BACK};
+	dat_info.valid = 0;
+	bat_info.valid = 0;
 	for (int i = 0; i < 2; i++) {
 		// Load the directory table.
 		int ret = loadDirTable(&mc_dat_int[i], DAT_addr[i], &mc_dat_chk_actual[i]);
@@ -469,7 +468,10 @@ int GcnCardPrivate::loadSysInfo(void)
 					 (mc_dat_int[i].dircntrl.chksum2);
 
 		// Check if the directory table is valid.
-		mc_dat_valid[i] = (mc_dat_chk_expected[i] == mc_dat_chk_actual[i]);
+		if (mc_dat_chk_expected[i] == mc_dat_chk_actual[i]) {
+			// DAT is valid.
+			dat_info.valid |= (1 << i);
+		}
 
 		// Load the block table.
 		ret = loadBlockTable(&mc_bat_int[i], BAT_addr[i], &mc_bat_chk_actual[i]);
@@ -485,7 +487,10 @@ int GcnCardPrivate::loadSysInfo(void)
 					 (mc_bat_int[i].chksum2);
 
 		// Check if the block table is valid.
-		mc_bat_valid[i] = (mc_bat_chk_expected[i] == mc_bat_chk_actual[i]);
+		if (mc_bat_chk_expected[i] == mc_bat_chk_actual[i]) {
+			// BAT is valid.
+			bat_info.valid |= (1 << i);
+		}
 	}
 
 	// Determine which tables are active.
@@ -591,10 +596,10 @@ int GcnCardPrivate::checkTables(void)
 	int idx = (mc_dat_int[1].dircntrl.updated > mc_dat_int[0].dircntrl.updated);
 
 	// Verify the checksums of the selected directory table.
-	if (!mc_dat_valid[idx]) {
+	if (!isDatValid(idx)) {
 		// Invalid checksum. Check the other directory table.
 		idx = !idx;
-		if (!mc_dat_valid[idx]) {
+		if (!isDatValid(idx)) {
 			// Both directory tables are invalid.
 			this->errors |= GcnCard::MCE_INVALID_DATS;
 			idx = -1;
@@ -604,7 +609,8 @@ int GcnCardPrivate::checkTables(void)
 	// Select the directory table.
 	int tmp_idx = (idx >= 0 ? idx : 0);
 	this->mc_dat = &mc_dat_int[tmp_idx];
-	this->mc_dat_hdr_idx = idx;
+	this->dat_info.active_hdr = idx;
+	this->dat_info.active = idx;
 
 	/**
 	 * Determine which block allocation table to use.
@@ -616,10 +622,10 @@ int GcnCardPrivate::checkTables(void)
 	idx = (mc_bat_int[1].updated > mc_bat_int[0].updated);
 
 	// Verify the checksums of the selected block allocation table.
-	if (!mc_bat_valid[idx]) {
+	if (!isBatValid(idx)) {
 		// Invalid checksum. Check the other block allocation table.
 		idx = !idx;
-		if (!mc_bat_valid[idx]) {
+		if (!isBatValid(idx)) {
 			// Both block allocation tables are invalid.
 			this->errors |= GcnCard::MCE_INVALID_BATS;
 			idx = -1;
@@ -629,7 +635,8 @@ int GcnCardPrivate::checkTables(void)
 	// Select the directory table.
 	tmp_idx = (idx >= 0 ? idx : 0);
 	this->mc_bat = &mc_bat_int[tmp_idx];
-	this->mc_bat_hdr_idx = idx;
+	this->bat_info.active_hdr = idx;
+	this->bat_info.active = idx;
 
 	// Update block counts.
 	totalUserBlocks = (totalPhysBlocks - 5);
@@ -772,6 +779,42 @@ GcnCard *GcnCard::format(const QString& filename, QObject *parent)
 	return gcnCard;
 }
 
+/** File system **/
+
+/**
+ * Set the active Directory Table index.
+ * NOTE: This function reloads the file list, without lost files.
+ * @param idx Active Directory Table index. (0 or 1)
+ */
+void GcnCard::setActiveDatIdx(int idx)
+{
+	if (!isOpen())
+		return;
+	Q_D(GcnCard);
+	if (idx < 0 || idx >= NUM_ELEMENTS(d->mc_dat_int))
+		return;
+	d->mc_dat = &d->mc_dat_int[idx];
+	d->loadGcnFileList();
+}
+
+/**
+ * Set the active Block Table index.
+ * NOTE: This function reloads the file list, without lost files.
+ * @param idx Active Block Table index. (0 or 1)
+ */
+void GcnCard::setActiveBatIdx(int idx)
+{
+	if (!isOpen())
+		return;
+	Q_D(GcnCard);
+	if (idx < 0 || idx >= NUM_ELEMENTS(d->mc_bat_int))
+		return;
+	d->mc_bat = &d->mc_bat_int[idx];
+	d->loadGcnFileList();
+}
+
+/** Card information **/
+
 /**
  * Get the product name of this memory card.
  * This refers to the class in general,
@@ -898,114 +941,4 @@ Checksum::ChecksumValue GcnCard::headerChecksumValue(void) const
 {
 	Q_D(const GcnCard);
 	return d->headerChecksumValue;
-}
-
-/**
- * Get the active Directory Table index.
- * @return Active Directory Table index. (0 or 1)
- */
-int GcnCard::activeDatIdx(void) const
-{
-	if (!isOpen())
-		return -1;
-	Q_D(const GcnCard);
-	return (d->mc_dat == &d->mc_dat_int[1]);
-}
-
-/**
- * Set the active Directory Table index.
- * NOTE: This function reloads the file list, without lost files.
- * @param idx Active Directory Table index. (0 or 1)
- */
-void GcnCard::setActiveDatIdx(int idx)
-{
-	if (!isOpen())
-		return;
-	Q_D(GcnCard);
-	if (idx < 0 || idx >= NUM_ELEMENTS(d->mc_dat_int))
-		return;
-	d->mc_dat = &d->mc_dat_int[idx];
-	d->loadGcnFileList();
-}
-
-/**
- * Get the active Directory Table index according to the card header.
- * @return Active Directory Table index (0 or 1), or -1 if both are invalid.
- */
-int GcnCard::activeDatHdrIdx(void) const
-{
-	if (!isOpen())
-		return -1;
-	Q_D(const GcnCard);
-	return d->mc_dat_hdr_idx;
-}
-
-/**
- * Is a Directory Table valid?
- * @param idx Directory Table index. (0 or 1)
- * @return True if valid; false if not valid or idx is invalid.
- */
-bool GcnCard::isDatValid(int idx) const
-{
-	if (!isOpen())
-		return false;
-	Q_D(const GcnCard);
-	if (idx < 0 || idx >= NUM_ELEMENTS(d->mc_dat_valid))
-		return false;
-	return d->mc_dat_valid[idx];
-}
-
-/**
- * Get the active Block Table index.
- * @return Active Block Table index. (0 or 1)
- */
-int GcnCard::activeBatIdx(void) const
-{
-	if (!isOpen())
-		return -1;
-	Q_D(const GcnCard);
-	return (d->mc_bat == &d->mc_bat_int[1]);
-}
-
-/**
- * Set the active Block Table index.
- * NOTE: This function reloads the file list, without lost files.
- * @param idx Active Block Table index. (0 or 1)
- */
-void GcnCard::setActiveBatIdx(int idx)
-{
-	if (!isOpen())
-		return;
-	Q_D(GcnCard);
-	if (idx < 0 || idx >= NUM_ELEMENTS(d->mc_bat_int))
-		return;
-	d->mc_bat = &d->mc_bat_int[idx];
-	d->loadGcnFileList();
-}
-
-/**
- * Get the active Block Table index according to the card header.
- * @return Active Block Table index (0 or 1), or -1 if both are invalid.
- */
-int GcnCard::activeBatHdrIdx(void) const
-{
-	if (!isOpen())
-		return -1;
-	Q_D(const GcnCard);
-	return d->mc_bat_hdr_idx;
-}
-
-/**
- * Is a Block Table valid?
- * @param idx Block Table index. (0 or 1)
- * @return True if valid; false if not valid or idx is invalid.
- */
-bool GcnCard::isBatValid(int idx) const
-{
-	if (!isOpen())
-		return false;
-	Q_D(const GcnCard);
-	if (idx < 0 || idx >= NUM_ELEMENTS(d->mc_bat_valid))
-		return false;
-	return d->mc_bat_valid[idx];
 }

src/mcrecover/card/GcnCard.hpp
@@ -42,10 +42,6 @@ class GcnCard : public Card
 
 	// TODO: Register Checksum::ChecksumValue metatype?
 	//Q_PROPERTY(Checksum::ChecksumValue headerChecksumValue READ headerChecksumValue)
-	Q_PROPERTY(int activeDatIdx READ activeDatIdx WRITE setActiveDatIdx)
-	Q_PROPERTY(int activeDatHdrIdx READ activeDatHdrIdx)
-	Q_PROPERTY(int activeBatIdx READ activeBatIdx WRITE setActiveBatIdx)
-	Q_PROPERTY(int activeBatHdrIdx READ activeBatHdrIdx)
 
 	protected:
 		GcnCard(QObject *parent = 0);
@@ -75,6 +71,25 @@ class GcnCard : public Card
 		static GcnCard *format(const QString& filename, QObject *parent);
 
 	public:
+		/** File system **/
+
+		/**
+		 * Set the active Directory Table index.
+		 * NOTE: This function reloads the file list, without lost files.
+		 * @param idx Active Directory Table index.
+		 */
+		virtual void setActiveDatIdx(int idx) override;
+
+		/**
+		 * Set the active Block Table index.
+		 * NOTE: This function reloads the file list, without lost files.
+		 * @param idx Active Block Table index.
+		 */
+		virtual void setActiveBatIdx(int idx) override;
+
+	public:
+		/** Card information **/
+
 		/**
 		 * Get the product name of this memory card.
 		 * This refers to the class in general,
@@ -119,58 +134,6 @@ class GcnCard : public Card
 		 * @return Header checksum value.
 		 */
 		Checksum::ChecksumValue headerChecksumValue(void) const;
-
-		/**
-		 * Get the active Directory Table index.
-		 * @return Active Directory Table index. (0 or 1)
-		 */
-		int activeDatIdx(void) const;
-
-		/**
-		 * Set the active Directory Table index.
-		 * NOTE: This function reloads the file list, without lost files.
-		 * @param idx Active Directory Table index. (0 or 1)
-		 */
-		void setActiveDatIdx(int idx);
-
-		/**
-		 * Get the active Directory Table index according to the card header.
-		 * @return Active Directory Table index (0 or 1), or -1 if both are invalid.
-		 */
-		int activeDatHdrIdx(void) const;
-
-		/**
-		 * Is a Directory Table valid?
-		 * @param idx Directory Table index. (0 or 1)
-		 * @return True if valid; false if not valid or idx is invalid.
-		 */
-		bool isDatValid(int idx) const;
-
-		/**
-		 * Get the active Block Table index.
-		 * @return Active Block Table index. (0 or 1)
-		 */
-		int activeBatIdx(void) const;
-
-		/**
-		 * Set the active Block Table index.
-		 * NOTE: This function reloads the file list, without lost files.
-		 * @param idx Active Block Table index. (0 or 1)
-		 */
-		void setActiveBatIdx(int idx);
-
-		/**
-		 * Get the active Block Table index according to the card header.
-		 * @return Active Block Table index (0 or 1), or -1 if both are invalid.
-		 */
-		int activeBatHdrIdx(void) const;
-
-		/**
-		 * Is a Block Table valid?
-		 * @param idx Block Table index. (0 or 1)
-		 * @return True if valid; false if not valid or idx is invalid.
-		 */
-		bool isBatValid(int idx) const;
 };
 
 #endif /* __MCRECOVER_CARD_GCNCARD_HPP__ */

src/mcrecover/card/VmuCard.cpp
@@ -110,7 +110,12 @@ class VmuCardPrivate : public CardPrivate
 };
 
 VmuCardPrivate::VmuCardPrivate(VmuCard *q)
-	: CardPrivate(q, VMU_BLOCK_SIZE, 256, 256)
+	: CardPrivate(q,
+		VMU_BLOCK_SIZE,	// 512-byte blocks.
+		256,	// Minimum card size, in blocks.
+		256,	// Maximum card size, in blocks.
+		1,	// Number of directory tables.
+		1)	// Number of block tables.
 {
 	// Make sure vmu.h is correct.
 	static_assert(VMU_BLOCK_SIZE == 512, "VMU_BLOCK_SIZE is incorrect");
@@ -201,6 +206,14 @@ int VmuCardPrivate::loadSysInfo(void)
 {
 	if (!file)
 		return -1;
+
+	// FIXME: Assuming 1 DAT and 1 BAT.
+	dat_info.active_hdr = 0;
+	dat_info.active = 0;
+	dat_info.valid = 1;
+	bat_info.active_hdr = 0;
+	bat_info.active = 0;
+	bat_info.valid = 1;
 
 	// Root block.
 	file->seek(VMU_ROOT_BLOCK_ADDRESS * blockSize);
@@ -493,6 +506,36 @@ VmuCard *VmuCard::format(const QString& filename, QObject *parent)
 	d->format(filename);
 	return vmuCard;
 }
+
+/** File system **/
+
+/**
+ * Set the active Directory Table index.
+ * NOTE: This function reloads the file list, without lost files.
+ * @param idx Active Directory Table index. (0 or 1)
+ */
+void VmuCard::setActiveDatIdx(int idx)
+{
+	if (!isOpen())
+		return;
+	// FIXME: Does DC have multiple directory tables?
+	Q_UNUSED(idx)
+}
+
+/**
+ * Set the active Block Table index.
+ * NOTE: This function reloads the file list, without lost files.
+ * @param idx Active Block Table index. (0 or 1)
+ */
+void VmuCard::setActiveBatIdx(int idx)
+{
+	if (!isOpen())
+		return;
+	// FIXME: Does DC have multiple block tables?
+	Q_UNUSED(idx)
+}
+
+/** Card information **/
 
 /**
  * Get the product name of this memory card.

src/mcrecover/card/VmuCard.hpp
@@ -36,6 +36,8 @@ class VmuCard : public Card
 
 	protected:
 		Q_DECLARE_PRIVATE(VmuCard)
+	private:
+		Q_DISABLE_COPY(VmuCard)
 
 	public:
 		/**
@@ -54,8 +56,22 @@ class VmuCard : public Card
 		 */
 		static VmuCard *format(const QString& filename, QObject *parent);
 
-	private:
-		Q_DISABLE_COPY(VmuCard)
+	public:
+		/** File system **/
+
+		/**
+		 * Set the active Directory Table index.
+		 * NOTE: This function reloads the file list, without lost files.
+		 * @param idx Active Directory Table index.
+		 */
+		virtual void setActiveDatIdx(int idx) override;
+
+		/**
+		 * Set the active Block Table index.
+		 * NOTE: This function reloads the file list, without lost files.
+		 * @param idx Active Block Table index.
+		 */
+		virtual void setActiveBatIdx(int idx) override;
 
 	public:
 		/**

commit 419f45ffbaf1f6f8fe670e3e2d18d42021344341

[mcrecover] Card: Set bat_info.count = bat_count, not dat_count.
This didn't affect anything in practice, since both GCN and VMU have
the same number of block tables as directory tables.

This fixes a regression from commit a4533a226de3d7f5b285ff0cb28567d1f09b5b33.
([mcrecover] Moved the block/directory table index functions down to the Card base class.)

src/mcrecover/card/Card.cpp
@@ -68,7 +68,7 @@ CardPrivate::CardPrivate(Card *q, uint32_t blockSize,
 	dat_info.valid = 0;
 
 	// Block Table information.
-	bat_info.count = dat_count;
+	bat_info.count = bat_count;
 	bat_info.active = -1;
 	bat_info.active_hdr = 0;
 	bat_info.valid = 0;