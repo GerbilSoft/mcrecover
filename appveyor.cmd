ababecf6e76c1a94f6d4f914c985a552e433378c
David Korth <gerbilsoft@gerbilsoft.com>
2018-04-14 11:54:06 -0400

    Fixes for travis-ci and appveyor.
    - travis-ci: Use Ubuntu 16.04. Ubuntu 14.04's qttools5-dev-tools is
      missing FindQt5LinguistTools.cmake.
    - appveyor: Set CMAKE_PREFIX_PATH again because Qt5_DIR isn't working.

--- appveyor.cmd
+++ appveyor.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF "%PLATFORM%" == "x64" (
-	cmake . -G "Visual Studio 12 2013 Win64" -DQt5_DIR=C:/Qt/5.10.1/msvc2013_64 -DQt5LinguistTools_DIR=C:/Qt/5.10.1/msvc2013_64
+	cmake . -G "Visual Studio 12 2013 Win64" -DCMAKE_PREFIX_PATH=C:/Qt/5.7/msvc2013_64
 ) ELSE (
-	cmake . -G "Visual Studio 12 2013" -DQt5_DIR=C:/Qt/5.7/msvc2013 -DQt5LinguistTools_DIR=C:/Qt/5.7/msvc2013
+	cmake . -G "Visual Studio 12 2013" -DCMAKE_PREFIX_PATH=C:/Qt/5.7/msvc2013
 )
