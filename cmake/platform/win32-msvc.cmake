GerbilSoft/mcrecover

commit cc9d7a10c199c668b21fbef198f932bd9d05d64c

Backported lots of CMake infrastructure changes from rom-properties.
Changes:
- Shortened CMake variable prefixes from "MCRECOVER_" to "MCR_".
- SET_WINDOWS_ENTRYPOINT()
- C11 checks (may be useful for _Static_assert() later)
- Thin archives on Linux.

cmake/platform/win32-msvc.cmake
@@ -4,8 +4,11 @@
 # Basic platform flags for MSVC:
 # - wchar_t should be a distinct type. (MSVC 2002+)
 IF(MSVC_VERSION GREATER 1200)
-	SET(MCRECOVER_C_FLAGS_WIN32 "${MCRECOVER_C_FLAGS_WIN32} -Zc:wchar_t")
+	SET(MCR_C_FLAGS_WIN32 "${MCR_C_FLAGS_WIN32} -Zc:wchar_t")
 ENDIF()
+
+# NOTE: This program is Unicode only on Windows.
+# No ANSI support.
 
 # Subsystem and minimum Windows version:
 # - If 32-bit: 5.00
@@ -19,26 +22,28 @@ ENDIF()
 # Use the SET_WINDOWS_SUBSYSTEM(_target _subsystem) function defined in
 # platform.cmake. That macro will only do anything if building with
 # MSVC, since the variables won't be defined anywhere else.
-IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+IF(CMAKE_CL_64)
 	# 64-bit, Unicode Windows only.
 	# (There is no 64-bit ANSI Windows.)
-	SET(MCRECOVER_C_FLAGS_WIN32 "${MCRECOVER_C_FLAGS_WIN32} -DWINVER=0x0502 -D_WIN32_WINNT=0x0502 -D_WIN32_IE=0x0600")
-	SET(MCRECOVER_LINKER_FLAGS_WIN32_EXE "/SUBSYSTEM:WINDOWS,5.02")
-	SET(MCRECOVER_LINKER_FLAGS_CONSOLE_EXE "/SUBSYSTEM:CONSOLE,5.02")
-ELSE()
+	# Minimum target version is Windows Server 2003 / XP 64-bit.
+	SET(MCR_WIN32_SUBSYSTEM_VERSION "5.02")
+ELSE(CMAKE_CL_64)
 	# 32-bit, Unicode Windows only.
-	SET(MCRECOVER_C_FLAGS_WIN32 "${MCRECOVER_C_FLAGS_WIN32} -DWINVER=0x0500 -D_WIN32_WINNT=0x0500 -D_WIN32_IE=0x0500")
 	IF(MSVC_VERSION GREATER 1600)
 		# MSVC 2012 or later.
 		# Minimum target version is Windows XP.
-		SET(MCRECOVER_LINKER_FLAGS_WIN32_EXE "/SUBSYSTEM:WINDOWS,5.01")
-		SET(MCRECOVER_LINKER_FLAGS_CONSOLE_EXE "/SUBSYSTEM:CONSOLE,5.01")
+		SET(MCR_WIN32_SUBSYSTEM_VERSION "5.01")
+		SET(MCR_LINKER_FLAGS_WIN32_EXE "/SUBSYSTEM:WINDOWS,5.01")
+		SET(MCR_LINKER_FLAGS_CONSOLE_EXE "/SUBSYSTEM:CONSOLE,5.01")
 	ELSE()
 		# MSVC 2010 or earlier.
-		SET(MCRECOVER_LINKER_FLAGS_WIN32_EXE "/SUBSYSTEM:WINDOWS,5.00")
-		SET(MCRECOVER_LINKER_FLAGS_CONSOLE_EXE "/SUBSYSTEM:CONSOLE,5.00")
+		# Minimum target version is Windows 2000.
+		SET(MCR_WIN32_SUBSYSTEM_VERSION "5.00")
 	ENDIF()
-ENDIF()
+ENDIF(CMAKE_CL_64)
+SET(MCR_LINKER_FLAGS_WIN32_EXE "/SUBSYSTEM:WINDOWS,${MCR_WIN32_SUBSYSTEM_VERSION}")
+SET(MCR_LINKER_FLAGS_CONSOLE_EXE "/SUBSYSTEM:CONSOLE,${MCR_WIN32_SUBSYSTEM_VERSION}")
+UNSET(MCR_WIN32_SUBSYSTEM_VERSION)
 
 # Release build: Prefer static libraries.
 # TODO: Use DLLs instead?
@@ -47,8 +52,8 @@ IF(CMAKE_BUILD_TYPE MATCHES ^release)
 ENDIF(CMAKE_BUILD_TYPE MATCHES ^release)
 
 # Append the CFLAGS and LDFLAGS.
-SET(MCRECOVER_C_FLAGS_COMMON			"${MCRECOVER_C_FLAGS_COMMON} ${MCRECOVER_C_FLAGS_WIN32}")
-SET(MCRECOVER_CXX_FLAGS_COMMON			"${MCRECOVER_CXX_FLAGS_COMMON} ${MCRECOVER_C_FLAGS_WIN32}")
+SET(MCR_C_FLAGS_COMMON			"${MCR_C_FLAGS_COMMON} ${MCR_C_FLAGS_WIN32}")
+SET(MCR_CXX_FLAGS_COMMON		"${MCR_CXX_FLAGS_COMMON} ${MCR_C_FLAGS_WIN32} ${MCR_CXX_FLAGS_WIN32}")
 
 # Unset temporary variables.
-UNSET(MCRECOVER_C_FLAGS_WIN32)
+UNSET(MCR_C_FLAGS_WIN32)

src/libgctools/CMakeLists.txt
@@ -8,9 +8,9 @@ INCLUDE_DIRECTORIES("${CMAKE_BINARY_DIR}")
 INCLUDE(TestBigEndian)
 TEST_BIG_ENDIAN(IS_BIG_ENDIAN)
 IF(IS_BIG_ENDIAN)
-        SET(MCRECOVER_BYTEORDER "MCRECOVER_BIG_ENDIAN")
+        SET(MCR_BYTEORDER "MCR_BIG_ENDIAN")
 ELSE(IS_BIG_ENDIAN)
-        SET(MCRECOVER_BYTEORDER "MCRECOVER_LIL_ENDIAN")
+        SET(MCR_BYTEORDER "MCR_LIL_ENDIAN")
 ENDIF(IS_BIG_ENDIAN)
 UNSET(IS_BIG_ENDIAN)
 CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/util/byteorder.h.in" "${CMAKE_CURRENT_BINARY_DIR}/util/byteorder.h")

src/mcrecover/CMakeLists.txt
@@ -7,8 +7,8 @@ INCLUDE_DIRECTORIES("${CMAKE_BINARY_DIR}")
 INCLUDE(DirInstallPaths)
 IF(WIN32 OR APPLE)
 	# Win32 and Mac OS X don't use `make install`.
-	SET(MCRECOVER_DATA_DIRECTORY "")
-	SET(MCRECOVER_TRANSLATIONS_DIRECTORY "")
+	SET(MCR_DATA_DIRECTORY "")
+	SET(MCR_TRANSLATIONS_DIRECTORY "")
 ELSE()
 	# Unix system.
 	SET(MCRECOVER_DATA_DIRECTORY "${CMAKE_INSTALL_PREFIX}/${DIR_INSTALL_DATA}")
@@ -419,6 +419,7 @@ ADD_DEPENDENCIES(mcrecover git_version)
 DO_SPLIT_DEBUG(mcrecover)
 SET_WINDOWS_SUBSYSTEM(mcrecover WIN32)
 SET_WINDOWS_NO_MANIFEST(mcrecover)
+SET_WINDOWS_ENTRYPOINT(mcrecover WinMain OFF)
 
 TARGET_INCLUDE_DIRECTORIES(mcrecover
 	PUBLIC	$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>

src/CMakeLists.txt
@@ -4,14 +4,14 @@ PROJECT(src)
 # NOTE: This must be included regardless of C++11 support in the compiler.
 # gcc-4.6 supports some C++11, but is missing explicit virtual overrides.
 IF(MSVC)
-	SET(MCRECOVER_CXX11_COMPAT_HEADER "-FI${CMAKE_CURRENT_SOURCE_DIR}/c++11-compat.h")
+	SET(MCR_CXX11_COMPAT_HEADER "-FI${CMAKE_CURRENT_SOURCE_DIR}/c++11-compat.h")
 ELSE(MSVC)
-	SET(MCRECOVER_CXX11_COMPAT_HEADER "-include ${CMAKE_CURRENT_SOURCE_DIR}/c++11-compat.h")
+	SET(MCR_CXX11_COMPAT_HEADER "-include ${CMAKE_CURRENT_SOURCE_DIR}/c++11-compat.h")
 ENDIF(MSVC)
 
 # Add the C++11 compatibility header to CFLAGS/CXXFLAGS.
-SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${MCRECOVER_CXX11_COMPAT_HEADER}")
-SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MCRECOVER_CXX11_COMPAT_HEADER}")
+SET(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${MCR_CXX11_COMPAT_HEADER}")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MCR_CXX11_COMPAT_HEADER}")
 
 # Source Code subdirectories.
 ADD_SUBDIRECTORY(libgctools)

src/gcbanner/CMakeLists.txt
@@ -50,6 +50,7 @@ INCLUDE(SetMSVCDebugPath)
 DO_SPLIT_DEBUG(gcbanner)
 SET_WINDOWS_SUBSYSTEM(gcbanner CONSOLE)
 SET_WINDOWS_NO_MANIFEST(gcbanner)
+SET_WINDOWS_ENTRYPOINT(gcbanner main ON)
 
 TARGET_INCLUDE_DIRECTORIES(gcbanner
 	PUBLIC	$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}>

src/miniu2d/CMakeLists.txt
@@ -16,3 +16,4 @@ ADD_EXECUTABLE(miniu2d
 	)
 DO_SPLIT_DEBUG(miniu2d)
 SET_WINDOWS_SUBSYSTEM(miniu2d CONSOLE)
+SET_WINDOWS_ENTRYPOINT(miniu2d main OFF)

cmake/macros/CheckC11C99CompilerFlag.cmake
@@ -1,0 +1,97 @@
+# - Check what flag is needed to activate C11 or C99 mode.
+# CHECK_C11_C99_COMPILER_FLAG(VARIABLE)
+#
+#  VARIABLE - variable to store the result
+#
+#  This actually calls the check_c_source_compiles macro.
+#  See help for CheckCSourceCompiles for a listing of variables
+#  that can modify the build.
+
+# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# Based on CHECK_C_COMPILER_FLAG(). (CheckCCompilerFlag.cmake)
+
+INCLUDE(CheckCSourceCompiles)
+
+MACRO(CHECK_C11_C99_COMPILER_FLAG _RESULT)
+	# Flag listing borrowed from GNU autoconf's AC_PROG_CC_C99 macro.
+	UNSET(${_RESULT})
+
+	# MSVC doesn't allow setting the C standard.
+	IF(NOT MSVC)
+	# Check if C11 is present without any flags.
+	# gcc-5.1 uses C11 mode by default.
+	MESSAGE(STATUS "Checking if C11 is enabled by default:")
+	CHECK_C_SOURCE_COMPILES("
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 201112L
+#error C11 is not enabled
+#endif
+
+int main() { return 0; }" CHECK_C11_ENABLED_DEFAULT)
+	IF (${CHECK_C11_ENABLED_DEFAULT})
+		UNSET(${_RESULT})
+		MESSAGE(STATUS "Checking if C11 is enabled by default: yes")
+	ELSE()
+		MESSAGE(STATUS "Checking if C11 is enabled by default: no")
+		MESSAGE(STATUS "Checking what CFLAG is required for C11:")
+		FOREACH(CHECK_C11_CFLAG "-std=gnu11" "-std=c11" "-c99" "-AC99" "-xc99=all" "-qlanglvl=extc1x" "-qlanglvl=stdc11")
+			SET(SAFE_CMAKE_REQUIRED_DEFINITIONS "${CMAKE_REQUIRED_DEFINITIONS}")
+			SET(CMAKE_REQUIRED_DEFINITIONS "${CHECK_C11_CFLAG}")
+			CHECK_C_SOURCE_COMPILES("int main() { return 0; }" CFLAG_${CHECK_C11_CFLAG})
+			SET(CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
+			IF(CFLAG_${CHECK_C11_CFLAG})
+				SET(${_RESULT} ${CHECK_C11_CFLAG})
+				BREAK()
+			ENDIF(CFLAG_${CHECK_C11_CFLAG})
+			UNSET(CFLAG_${CHECK_C11_CFLAG})
+		ENDFOREACH()
+
+		IF(${_RESULT})
+			MESSAGE(STATUS "Checking what CFLAG is required for C11: ${${_RESULT}}")
+		ELSE(${_RESULT})
+			MESSAGE(STATUS "Checking what CFLAG is required for C11: unavailable")
+		ENDIF(${_RESULT})
+	ENDIF()
+
+	IF(NOT CHECK_C11_ENABLED_DEFAULT AND NOT ${_RESULT})
+		# Could not enable C11. Try C99 instead.
+		MESSAGE(STATUS "Checking if C99 is enabled by default:")
+		CHECK_C_SOURCE_COMPILES("
+	#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
+	#error C99 is not enabled
+	#endif
+
+	int main() { return 0; }" CHECK_C99_ENABLED_DEFAULT)
+		IF (${CHECK_C99_ENABLED_DEFAULT})
+			UNSET(${_RESULT})
+			MESSAGE(STATUS "Checking if C99 is enabled by default: yes")
+		ELSE()
+			MESSAGE(STATUS "Checking if C99 is enabled by default: no")
+			MESSAGE(STATUS "Checking what CFLAG is required for C99:")
+			FOREACH(CHECK_C99_CFLAG "-std=gnu99" "-std=c99" "-c99" "-AC99" "-xc99=all" "-qlanglvl=extc99")
+				SET(SAFE_CMAKE_REQUIRED_DEFINITIONS "${CMAKE_REQUIRED_DEFINITIONS}")
+				SET(CMAKE_REQUIRED_DEFINITIONS "${CHECK_C99_CFLAG}")
+				CHECK_C_SOURCE_COMPILES("int main() { return 0; }" CFLAG_${CHECK_C99_CFLAG})
+				SET(CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
+				IF(CFLAG_${CHECK_C99_CFLAG})
+					SET(${_RESULT} ${CHECK_C99_CFLAG})
+					BREAK()
+				ENDIF(CFLAG_${CHECK_C99_CFLAG})
+				UNSET(CFLAG_${CHECK_C99_CFLAG})
+			ENDFOREACH()
+
+			IF(${_RESULT})
+				MESSAGE(STATUS "Checking what CFLAG is required for C99: ${${_RESULT}}")
+			ELSE(${_RESULT})
+				MESSAGE(STATUS "Checking what CFLAG is required for C99: unavailable")
+			ENDIF(${_RESULT})
+		ENDIF()
+	ENDIF(NOT CHECK_C11_ENABLED_DEFAULT AND NOT ${_RESULT})
+
+	UNSET(CHECK_C11_ENABLED_DEFAULT)
+	UNSET(CHECK_C99_ENABLED_DEFAULT)
+	ENDIF(NOT MSVC)
+ENDMACRO(CHECK_C11_C99_COMPILER_FLAG)

cmake/macros/CheckC99CompilerFlag.cmake
@@ -1,60 +1,0 @@
-# - Check what flag is needed to activate C99 mode.
-# CHECK_C99_COMPILER_FLAG(VARIABLE)
-#
-#  VARIABLE - variable to store the result
-# 
-#  This actually calls the check_c_source_compiles macro.
-#  See help for CheckCSourceCompiles for a listing of variables
-#  that can modify the build.
-
-# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-
-# Based on CHECK_C_COMPILER_FLAG(). (CheckCCompilerFlag.cmake)
-
-INCLUDE(CheckCSourceCompiles)
-
-MACRO(CHECK_C99_COMPILER_FLAG _RESULT)
-	# Flag listing borrowed from GNU autoconf's AC_PROG_CC_C99 macro.
-	UNSET(${_RESULT})
-
-	# MSVC doesn't allow setting the C standard.
-	IF(NOT MSVC)
-	# Check if C99 is present without any flags.
-	# gcc-5.1 uses C11 mode by default.
-	MESSAGE(STATUS "Checking if C99 is enabled by default:")
-	CHECK_C_SOURCE_COMPILES("
-#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
-#error C99 is not enabled
-#endif
-
-int main() { return 0; }" CHECK_C99_ENABLED_DEFAULT)
-	IF (${CHECK_C99_ENABLED_DEFAULT})
-		UNSET(${_RESULT})
-		MESSAGE(STATUS "Checking if C99 is enabled by default: yes")
-	ELSE()
-		MESSAGE(STATUS "Checking if C99 is enabled by default: no")
-		MESSAGE(STATUS "Checking what CFLAG is required for C99:")
-		FOREACH(CHECK_C99_CFLAG "-std=gnu99" "-std=c99" "-c99" "-AC99" "-xc99=all" "-qlanglvl=extc99")
-			SET(SAFE_CMAKE_REQUIRED_DEFINITIONS "${CMAKE_REQUIRED_DEFINITIONS}")
-			SET(CMAKE_REQUIRED_DEFINITIONS "${CHECK_C99_CFLAG}")
-			CHECK_C_SOURCE_COMPILES("int main() { return 0; }" CFLAG_${CHECK_C99_CFLAG})
-			SET(CMAKE_REQUIRED_DEFINITIONS "${SAFE_CMAKE_REQUIRED_DEFINITIONS}")
-			IF(CFLAG_${CHECK_C99_CFLAG})
-				SET(${_RESULT} ${CHECK_C99_CFLAG})
-				BREAK()
-			ENDIF(CFLAG_${CHECK_C99_CFLAG})
-			UNSET(CFLAG_${CHECK_C99_CFLAG})
-		ENDFOREACH()
-
-		IF(${_RESULT})
-			MESSAGE(STATUS "Checking what CFLAG is required for C99: ${${_RESULT}}")
-		ELSE(${_RESULT})
-			MESSAGE(STATUS "Checking what CFLAG is required for C99: none")
-		ENDIF(${_RESULT})
-	ENDIF()
-	UNSET(CHECK_C99_ENABLED_DEFAULT)
-	ENDIF(NOT MSVC)
-ENDMACRO (CHECK_C99_COMPILER_FLAG)

commit ca10c83c98f8a63f1eafd9e1849aacfca33bb7c8

Fixed a few overzealous MCRECOVER -> MCR changes.
This ended up breaking endianness checks, which broke everything.

This fixes a regression from commit cc9d7a10c199c668b21fbef198f932bd9d05d64c.
(Backported lots of CMake infrastructure changes from rom-properties.)

cmake/platform/win32-msvc.cmake
@@ -33,8 +33,6 @@ ELSE(CMAKE_CL_64)
 		# MSVC 2012 or later.
 		# Minimum target version is Windows XP.
 		SET(MCR_WIN32_SUBSYSTEM_VERSION "5.01")
-		SET(MCR_LINKER_FLAGS_WIN32_EXE "/SUBSYSTEM:WINDOWS,5.01")
-		SET(MCR_LINKER_FLAGS_CONSOLE_EXE "/SUBSYSTEM:CONSOLE,5.01")
 	ELSE()
 		# MSVC 2010 or earlier.
 		# Minimum target version is Windows 2000.

src/libgctools/CMakeLists.txt
@@ -8,9 +8,9 @@ INCLUDE_DIRECTORIES("${CMAKE_BINARY_DIR}")
 INCLUDE(TestBigEndian)
 TEST_BIG_ENDIAN(IS_BIG_ENDIAN)
 IF(IS_BIG_ENDIAN)
-        SET(MCR_BYTEORDER "MCR_BIG_ENDIAN")
+        SET(MCRECOVER_BYTEORDER "MCRECOVER_BIG_ENDIAN")
 ELSE(IS_BIG_ENDIAN)
-        SET(MCR_BYTEORDER "MCR_LIL_ENDIAN")
+        SET(MCRECOVER_BYTEORDER "MCRECOVER_LIL_ENDIAN")
 ENDIF(IS_BIG_ENDIAN)
 UNSET(IS_BIG_ENDIAN)
 CONFIGURE_FILE("${CMAKE_CURRENT_SOURCE_DIR}/util/byteorder.h.in" "${CMAKE_CURRENT_BINARY_DIR}/util/byteorder.h")

src/mcrecover/CMakeLists.txt
@@ -7,8 +7,8 @@ INCLUDE_DIRECTORIES("${CMAKE_BINARY_DIR}")
 INCLUDE(DirInstallPaths)
 IF(WIN32 OR APPLE)
 	# Win32 and Mac OS X don't use `make install`.
-	SET(MCR_DATA_DIRECTORY "")
-	SET(MCR_TRANSLATIONS_DIRECTORY "")
+	SET(MCRECOVER_DATA_DIRECTORY "")
+	SET(MCRECOVER_TRANSLATIONS_DIRECTORY "")
 ELSE()
 	# Unix system.
 	SET(MCRECOVER_DATA_DIRECTORY "${CMAKE_INSTALL_PREFIX}/${DIR_INSTALL_DATA}")