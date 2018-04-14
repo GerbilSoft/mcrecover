cc9d7a10c199c668b21fbef198f932bd9d05d64c
David Korth <gerbilsoft@gerbilsoft.com>
2018-04-14 15:01:17 -0400

    Backported lots of CMake infrastructure changes from rom-properties.
    Changes:
    - Shortened CMake variable prefixes from "MCRECOVER_" to "MCR_".
    - SET_WINDOWS_ENTRYPOINT()
    - C11 checks (may be useful for _Static_assert() later)
    - Thin archives on Linux.

--- cmake/macros/DirInstallPaths.cmake
+++ cmake/macros/DirInstallPaths.cmake
@@ -11,11 +11,28 @@ IF(NOT PACKAGE_NAME)
 	MESSAGE(FATAL_ERROR "PACKAGE_NAME is not set.")
 ENDIF(NOT PACKAGE_NAME)
 
+# Architecture name for arch-specific paths.
+STRING(TOLOWER "${CMAKE_SYSTEM_PROCESSOR}" arch)
+IF(arch MATCHES "^(i.|x)86$|^x86_64$|^amd64$")
+	# i386/amd64. Check sizeof(void*) for the actual architecture,
+	# since building 32-bit on 64-bit isn't considered "cross-compiling",
+	# so CMAKE_SYSTEM_PROCESSOR might not be accurate.
+	# NOTE: Checking CMAKE_CL_64 instead of sizeof(void*) for MSVC builds.
+	IF(MSVC AND CMAKE_CL_64)
+		SET(arch "amd64")
+	ELSEIF(NOT MSVC AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+		SET(arch "amd64")
+	ELSE()
+		SET(arch "i386")
+	ENDIF()
+ENDIF(arch MATCHES "^(i.|x)86$|^x86_64$|^amd64$")
+
+INCLUDE(GNUInstallDirs)
 IF(UNIX AND NOT APPLE)
 	# Unix-style install paths.
-	SET(DIR_INSTALL_EXE "bin")
-	SET(DIR_INSTALL_DLL "lib")
-	SET(DIR_INSTALL_LIB "lib")
+	SET(DIR_INSTALL_EXE "${CMAKE_INSTALL_BINDIR}")
+	SET(DIR_INSTALL_DLL "${CMAKE_INSTALL_LIBDIR}")
+	SET(DIR_INSTALL_LIB "${CMAKE_INSTALL_LIBDIR}")
 	SET(DIR_INSTALL_DATA "share/${PACKAGE_NAME}/data")
 	SET(DIR_INSTALL_TRANSLATIONS "share/${PACKAGE_NAME}/translations")
 	SET(DIR_INSTALL_DOC "share/doc/${PACKAGE_NAME}")
@@ -26,7 +43,7 @@ IF(UNIX AND NOT APPLE)
 ELSEIF(APPLE)
 	# Mac OS X-style install paths.
 	# Install should be relative to the application bundle.
-	# (TODO when adding Mac OS support.)
+	# TODO: Not supported...
 	MESSAGE(STATUS "WARNING: Mac OS X is not officially supported yet.")
 ELSEIF(WIN32)
 	# Win32-style install paths.
@@ -39,9 +56,6 @@ ELSEIF(WIN32)
 	SET(DIR_INSTALL_TRANSLATIONS "translations")
 	SET(DIR_INSTALL_DOC "doc")
 	SET(DIR_INSTALL_DOC_ROOT ".")
-	SET(DIR_INSTALL_EXE_DEBUG "debug")
-	SET(DIR_INSTALL_DLL_DEBUG "debug")
-	SET(DIR_INSTALL_LIB_DEBUG "debug")
 	# Installing debug symbols for DLLs in the
 	# same directory as the DLL.
 	SET(DIR_INSTALL_EXE_DEBUG "${DIR_INSTALL_EXE}")
