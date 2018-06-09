GerbilSoft/mcrecover

commit d07d77d59dce2dec0951e447c154c29d8b6cb36d

[libmemcard] MemCardModel::columnCount(): Return 0 if no card is set.
Copied over from qrvthtool.

src/libmemcard/MemCardModel.cpp
@@ -280,7 +280,10 @@ int MemCardModel::rowCount(const QModelIndex& parent) const
 int MemCardModel::columnCount(const QModelIndex& parent) const
 {
 	Q_UNUSED(parent);
-	return COL_MAX;
+	if (d->card) {
+		return COL_MAX;
+	}
+	return 0;
 }
 
 QVariant MemCardModel::data(const QModelIndex& index, int role) const

commit 774d107ae52028bc9b4ddf4d200d2ae14dda4d40

[libmemcard] MemCardModel::columnCount(): Added Q_D().
This fixes a regression from commit d07d77d59dce2dec0951e447c154c29d8b6cb36d.
([libmemcard] MemCardModel::columnCount(): Return 0 if no card is set.)

src/libmemcard/MemCardModel.cpp
@@ -280,6 +280,7 @@ int MemCardModel::rowCount(const QModelIndex& parent) const
 int MemCardModel::columnCount(const QModelIndex& parent) const
 {
 	Q_UNUSED(parent);
+	Q_D(const MemCardModel);
 	if (d->card) {
 		return COL_MAX;
 	}